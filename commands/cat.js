0045ae35aa9a77e0f1cff274c0bfee08d0eaeadb
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-08-31 18:20:08 +0100

    added cat

---
+++ commands/cat.js
@@ -0,0 +1,30 @@
+const { MessageEmbed, Message } = require("discord.js")
+const fetch = require('node-fetch');
+
+module.exports.run = async (bot, message, args) => {
+
+    let msg = await message.channel.send("Generating Cuteness...")
+
+    fetch("http://aws.random.cat/meow")
+        .then(res => res.json()).then(body => {
+            if (!body) return message.reply(" Unable to load!")
+
+            let embed = new MessageEmbed()
+                .setColor(Orange)
+                .setAuthor(`${bot.user.username} Dogs!`, message.guild.iconURL)
+                .setImage(body.file)
+                .setTimestamp()
+                .setFooter(bot.user.username.toUpperCase(), bot.user.displayAvatarURL)
+
+            msg.edit(embed)
+        })
+}
+
+module.exports.config = {
+    name: "cat",
+    description: "sends a picture of a cat",
+    usage: "-cat",
+    category: "images",
+    accessableby: "Members",
+    aliases: ["cutecat"]
+}
