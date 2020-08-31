32d980902cded2cd73ad0c8b9a17ed263639961e
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-08-31 20:06:11 +0100

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -1,28 +1,28 @@
-const { MessageEmbed, Message } = require("discord.js");
+const { MessageEmbed, Message } = require("discord.js")
 const fetch = require('node-fetch');
 
 module.exports.run = async (bot, message, args) => {
+
     let msg = await message.channel.send("Generating Cuteness...")
 
-    fetch(`https://dog.ceo/api/breeds/image/random`)
-        .then(res => res.json()), then(body => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json()).then(body => {
             if (!body) return message.reply(" Unable to load image!")
 
             let embed = new MessageEmbed()
                 .setColor('#FFA500')
                 .setAuthor(`${bot.user.username}: Doggos!`, message.guild.iconURL)
-                .setIMage(body.file)
+                .setImage(body.file)
                 .setTimestamp()
                 .setFooter(bot.user.username.toUpperCase(), bot.user.displayAvatarURL)
 
             msg.edit(embed)
-            msg.delete();
         })
 }
 
 module.exports.config = {
     name: "dog",
-    description: "sends pictures of dogs",
+    description: "sends a picture of a cat",
     usage: "-dog",
     category: "images",
     accessableby: "Members",
