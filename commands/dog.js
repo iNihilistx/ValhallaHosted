4b4643eefc4160257b3ba7695cba16e689189a07
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-08-31 20:14:35 +0100

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -9,14 +9,14 @@ module.exports.run = async (bot, message, args) => {
         .then(res => res.json()).then(body => {
             if (!body) return message.reply(" Unable to load image!")
 
-            let embed = new MessageEmbed()
+            let dembed = new MessageEmbed()
                 .setColor('#FFA500')
                 .setAuthor(`${bot.user.username}: Doggos!`, message.guild.iconURL)
                 .setImage(body.file)
                 .setTimestamp()
                 .setFooter(bot.user.username.toUpperCase(), bot.user.displayAvatarURL)
 
-            msg.edit(embed)
+            msg.edit(dembed)
         })
 }
 
