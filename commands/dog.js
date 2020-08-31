09474bbf77d10d962a4bc0c862ded6a74e57f2ea
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-08-31 19:58:55 +0100

    Create dog.js

---
+++ commands/dog.js
@@ -0,0 +1,29 @@
+const { MessageEmbed, Message } = require("discord.js");
+const fetch = require('node-fetch');
+
+module.exports.run = async (bot, message, args) => {
+    let msg = await message.channel.send("Generating Cuteness...")
+
+    fetch(`https://dog.ceo/api/breeds/image/random`)
+        .then(res => res.json()), then(body => {
+            if (!body) return message.reply(" Unable to load image!")
+
+            let embed = new MessageEmbed()
+                .setColor('#FFA500')
+                .setAuthor(`${bot.user.username}: Doggos!`, message.guild.iconURL)
+                .setIMage(body.file)
+                .setTimestamp()
+                .setFooter(bot.user.username.toUpperCase(), bot.user.displayAvatarURL)
+
+            msg.edit(embed)
+        })
+}
+
+module.exports.confid = {
+    name: "dog",
+    description: "sends pictures of dogs",
+    usage: "-dog",
+    category: "images",
+    accessableby: "Members",
+    aliases: ["doggos"]
+}
