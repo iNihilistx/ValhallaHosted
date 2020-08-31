0e7527562572dc850e48509fd8dab7c8ac1f7904
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-08-31 19:18:25 +0100

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -1,26 +1,26 @@
 module.exports.run = async (bot, message, args) => {
 
     message.delete()
-    //this will mention or grab the user
-    let target = message.mentions.members.first() || message.guild.members.get(ars[0])
-    if (target) return message.channel.send("You appear to have mentioned an invalid user!").then(m => m.delte(15000))
+    // mentioned or grabbed user
+    let target = message.mentions.members.first() || message.guild.members.get(args[0])
+    if (!target) return message.channel.send("Please provide a valid user").then(m => m.delete(15000))
 
-    // the reasoning
+    // reasoning definition
     let reason = args.slice(1).join(" ")
-    if (!reason) return message.channel.send(`Please provide a reason for the report **${target.user.tag}`).then(m => m.delte(15000))
+    if (!reason) return message.channel.send(`Please provide a reason for reporting **${target.user.tag}**`).then(m => m.delete(15000))
 
-    //grabs the report channel to send the report
-    let sChannel = message.guild.channels.find(x => x.name === "reports")
+    // grab reports channel
+    let sChannel = message.guild.channels.find(x => x.name === "tut-reports")
 
-    //send to the report channel and add a tick and cross
+    // send to reports channel and add tick or cross
 
-    message.channel.send("Your report has been sent to the Moderators. Thank you!").then(m => m.delete(15000))
-    sChannel.send(`**${message.author.tag}** has reported **${target.user.tag}** for **${reason}**!`).then(async msg => {
+    message.channel.send("Your report has been filed to the staff team. Thank you!").then(m => m.delete(15000))
+    sChannel.send(`**${message.author.tag}** has reported **${target.user.tag}** for **${reason}**.`).then(async msg => {
         await msg.react("✅")
         await msg.react("❌")
     })
-}
 
+}
 module.exports.config = {
     name: "report",
     description: "reports a member within the guild",
