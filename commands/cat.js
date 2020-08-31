8573ff89965343530a3f901dc8f87cf7e83d7c27
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-08-31 20:13:35 +0100

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -5,18 +5,18 @@ module.exports.run = async (bot, message, args) => {
 
     let msg = await message.channel.send("Generating Cuteness...")
 
-    fetch("http://aws.random.cat/meow")
+    fetch('http://aws.random.cat/meow')
         .then(res => res.json()).then(body => {
             if (!body) return message.reply(" Unable to load image!")
 
-            let embed = new MessageEmbed()
+            let cembed = new MessageEmbed()
                 .setColor('#FFA500')
                 .setAuthor(`${bot.user.username}: Cattos!`, message.guild.iconURL)
                 .setImage(body.file)
                 .setTimestamp()
                 .setFooter(bot.user.username.toUpperCase(), bot.user.displayAvatarURL)
 
-            msg.edit(embed)
+            msg.edit(cembed)
         })
 }
 
