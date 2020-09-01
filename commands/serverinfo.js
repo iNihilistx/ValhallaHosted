53cf1d3ad2b9d0a5bb0bd60f299d544ec7fe9a82
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-01 22:33:43 +0100

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -4,11 +4,11 @@ const botconfig = require("../config.json");
 module.exports.run = async (bot, message, args) => {
 
     message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-
-    module.exports.config = {
-        name: "serverinfo",
-        description: "shows server information",
-        usage: "-serverinfo",
-        accessableby: "Members",
-        aliases: []
-    }
+}
+module.exports.config = {
+    name: "serverinfo",
+    description: "shows server information",
+    usage: "-serverinfo",
+    accessableby: "Members",
+    aliases: []
+}
