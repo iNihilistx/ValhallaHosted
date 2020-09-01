4aeb437f4d19e615ee5aa341b1fbfbd5eff5de3c
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-01 23:35:06 +0100

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
         .addFields(
             { name: "Server Name: ", value: `${message.guild.name}` },
             { name: "Current Members: ", value: `${message.guild.memberCount}` },
-            { name: 'Server Owner', value: '285691402405281792', inline: true },
+            { name: 'Server Owner', value: 'Flash', inline: true },
             { name: 'Your Username', value: `${message.author.username}`, inline: true }
         )
         .addField('Valhalla')
