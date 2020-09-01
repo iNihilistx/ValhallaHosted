de40c42eefa6f430cbe406c855a10d6de3bcd399
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-01 20:26:55 +0100

    arole, rrole

---
+++ commands/addrole.js
@@ -0,0 +1,20 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let user = message.mentions.members.first()
+    if (!user) return message.channel.send("Unable to find user")
+
+    let role = message.mentions.roles.first()
+    if (!role) return message.channel.send("Role is missing")
+
+    message.channel.send(`${user} now has the ${role} role`)
+    user.roles.add(role)
+}
+
+module.exports.help = {
+    name: "addrole",
+    description: "adds roles",
+    usage: "-addrole",
+    accessableby: "moderator",
+    aliases: ["arole"]
+}
