9a2b1b0bbe616932c1a1a92c06dde673cfa3219e
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-01 22:45:30 +0100

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -2,8 +2,20 @@ const Discord = require("discord.js")
 const botconfig = require("../config.json");
 
 module.exports.run = async (bot, message, args) => {
+    const ServerEmbed = new Discord.MessageEmbed()
+        .setColor('#FFA500')
+        .setTitle('Server Statistics')
+        .setURL('https://discord.js.org/')
+        .setAuthor('Nairobi')
+        .setDescription(`Server Statistics for ${message.guild.name}`)
+        .setThumbnail('https:/i.imgur.com/wSTFkRM.png')
+        .addFields(
+            { name: 'Server Name: ', value: `${message.guild.name}` },
+            { name: 'Member Count: ', value: `${message.guild.memberCount}` }
+        )
 
-    message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+
+
 }
 module.exports.config = {
     name: "serverinfo",
