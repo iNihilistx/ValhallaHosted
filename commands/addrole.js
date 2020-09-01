90114c18c5ce959ab31c38554e6259cb6fb455bb
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-01 20:33:59 +0100

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -3,12 +3,12 @@ const botconfig = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
     let user = message.mentions.members.first()
-    if (!user) return message.channel.send("Unable to find user")
+    if (!member) return message.channel.send("Unable to find member")
 
     let role = message.mentions.roles.first()
     if (!role) return message.channel.send("Role is missing")
 
-    message.channel.send(`${user} role has been removed (${role})`)
+    message.channel.send(`${member} role has been removed (${role})`)
     user.roles.add(role)
 }
 
