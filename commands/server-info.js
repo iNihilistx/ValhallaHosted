8fb6abbbfb9068a344977b71c6372f2c9b8f4ba2
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-02 23:59:30 +0100

    Update server-info.js

--- commands/server-info.js
+++ commands/server-info.js
@@ -2,6 +2,13 @@ const Discord = require("discord.js")
 const botconfig = require("../config.json");
 
 module.exports.run = async (bot, message, args) => {
+    function duration(ms) {
+        const sec = Math.floor((ms / 1000) % 60).toString()
+        const min = Math.floor((ms / (1000 * 60)) % 60).toString()
+        const hrs = Math.floor((ms / (1000 * 60 * 60)) % 60).toString()
+        const days = Math.floor((ms / (1000 * 60 * 60 * 24)) % 60).toString()
+        return `${days.padStart(1, '0')} days, ${hrs.padStart(2, '0')} hours, ${min.padStart(2, '0')} minutes, ${sec.padStart(2, '0')}seconds.`
+    }
     const serverinfoEmbed = new Discord.MessageEmbed()
         .setColor('#FFA500')
         .setTitle('Server Information: ')
@@ -10,12 +17,13 @@ module.exports.run = async (bot, message, args) => {
         .setDescription(`Server information`)
         .setThumbnail('https://i.imgur.com/G5bui5n.png')
         .addFields(
-            { name: "Server Name: ", value: `${message.guild.name}` },
-            { name: "Current Members: ", value: `${message.guild.memberCount}` },
-            { name: 'Requested By: ', value: `${message.member.user.tag}`, inline: true },
-            { name: 'Server Owner: ', value: `${message.guild.owner}`, inline: true }
+            { name: "Server Name: ", value: `${message.guild.name} ` },
+            { name: "Current Members: ", value: `${message.guild.memberCount} ` },
+            { name: 'Requested By: ', value: `${message.member.user.tag} `, inline: true },
+            { name: 'Server Owner: ', value: `${message.guild.owner} `, inline: true }
         )
         .addField("Server Region: ", message.guild.region, true)
+        .addField("Bot Uptime: ", message.channel.send`${duration(bot.uptime)}`)
         .setImage(message.guild.iconURL())
         .setTimestamp()
         .setFooter('Valhalla, created by: Nairobi', 'https://i.imgur.com/G5bui5n.png')
