7144180b2b286ed1cf338304ce630a5fe7091422
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-03 00:03:09 +0100

    Update server-info.js

--- commands/server-info.js
+++ commands/server-info.js
@@ -2,14 +2,14 @@ const Discord = require("discord.js")
 const botconfig = require("../config.json");
 
 module.exports.run = async (bot, message, args) => {
-    function duration(ms) {
-        const sec = Math.floor((ms / 1000) % 60).toString()
-        const min = Math.floor((ms / (1000 * 60)) % 60).toString()
-        const hrs = Math.floor((ms / (1000 * 60 * 60)) % 60).toString()
-        const days = Math.floor((ms / (1000 * 60 * 60 * 24)) % 60).toString()
-        return `${days.padStart(1, '0')} days, ${hrs.padStart(2, '0')} hours, ${min.padStart(2, '0')} minutes, ${sec.padStart(2, '0')}seconds.`
-    }
     const serverinfoEmbed = new Discord.MessageEmbed()
+    function duration(ms)
+    const sec = Math.floor((ms / 1000) % 60).toString()
+    const min = Math.floor((ms / (1000 * 60)) % 60).toString()
+    const hrs = Math.floor((ms / (1000 * 60 * 60)) % 60).toString()
+    const days = Math.floor((ms / (1000 * 60 * 60 * 24)) % 60).toString()
+    return `${days.padStart(1, '0')} days, ${hrs.padStart(2, '0')} hours, ${min.padStart(2, '0')} minutes, ${sec.padStart(2, '0')}seconds.`
+
         .setColor('#FFA500')
         .setTitle('Server Information: ')
         .setURL('https://i.imgur.com/G5bui5n.png')
