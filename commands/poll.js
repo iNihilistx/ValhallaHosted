83bb291016bf14fc5961f3668c535b0958a10a65
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-03 22:33:40 +0100

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -1,22 +1,33 @@
 const Discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-    let pollChannel = message.mentions.channels.first();
-    let pollDescription = args.slice(1).join(' ');
-
-    let embedPoll = new Discord.MessageEmbed()
-        .setTitle('New Poll!')
-        .setDescription(pollDescription)
-        .setColor('#FFA500')
-    let msgEmbed = await pollChannel.send(embedPoll);
-    await msgEmbed.react('👍')
-    await msgEmbed.react('👎')
-
-}
-module.exports.config = {
-    name: "Poll",
-    description: "",
-    usage: "-poll",
-    accessableby: "Members",
-    aliases: []
-}
+module.exports = {
+    name: "poll",
+    description: "Create a simple yes or no poll",
+    category: "fun",
+    run: async (bot, message, args) => {
+        if (!message.member.permissions.has("MODERATOR"))
+            return message.channel.send(
+                `You do not have admin, ${message.author.username}`
+            );
+        const channel =
+            message.mentions.channels.first() ||
+            message.guild.channels.cache.get(args[0]);
+        if (!channel) {
+            return message.channel.send(
+                `You did not mention / give the id of your channel!`
+            );
+        }
+        let question = message.content
+            .split(`${bot.prefix}poll ${channel} `)
+            .join("");
+        if (!question)
+            return message.channel.send(`You did not specify your question!`);
+        const Embed = new Discord.MessageEmbed()
+            .setTitle(`New poll!`)
+            .setDescription(`${question}`)
+            .setFooter(`${message.author.username} created this poll.`)
+            .setColor(`RANDOM`);
+        let msg = await bot.channels.cache.get(channel.id).send(Embed);
+        await msg.react("👍");
+        await msg.react("👎");
+    },
+};
