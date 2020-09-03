47f35ac49b418358104c581cb2de529554b7a8de
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-03 22:37:37 +0100

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -1,33 +1,26 @@
-const Discord = require("discord.js");
-module.exports = {
+module.exports.run = async (bot, message, args) => {
+    if (message.author.bot || message.channel.type === "dm") return;
+
+    const messageArray = message.content.split(' ');
+    const cmd = messageArray[0];
+    const args = messageArray.slice(1);
+
+    let pollChannel = message.mentions.channels.first();
+    let pollDescription = args.slice(1).join(' ');
+
+    let embedPoll = new Discord.MessageEmbed()
+        .setTitle('😲 New Poll! 😲')
+        .setDescription(pollDescription)
+        .setColor('YELLOW')
+    let msgEmbed = await pollChannel.send(embedPoll);
+    await msgEmbed.react('👍')
+    await msgEmbed.react('👎')
+}
+
+module.exports.config = {
     name: "poll",
-    description: "Create a simple yes or no poll",
-    category: "fun",
-    run: async (bot, message, args) => {
-        if (!message.member.permissions.has("MODERATOR"))
-            return message.channel.send(
-                `You do not have admin, ${message.author.username}`
-            );
-        const channel =
-            message.mentions.channels.first() ||
-            message.guild.channels.cache.get(args[0]);
-        if (!channel) {
-            return message.channel.send(
-                `You did not mention / give the id of your channel!`
-            );
-        }
-        let question = message.content
-            .split(`${bot.prefix}poll ${channel} `)
-            .join("");
-        if (!question)
-            return message.channel.send(`You did not specify your question!`);
-        const Embed = new Discord.MessageEmbed()
-            .setTitle(`New poll!`)
-            .setDescription(`${question}`)
-            .setFooter(`${message.author.username} created this poll.`)
-            .setColor(`RANDOM`);
-        let msg = await bot.channels.cache.get(channel.id).send(Embed);
-        await msg.react("👍");
-        await msg.react("👎");
-    },
-};
+    description: "poll",
+    usage: "-poll",
+    accessableby: "Members",
+    aliases: [""]
+}
