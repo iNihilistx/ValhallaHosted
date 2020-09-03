a893ce73d9ba91301e27b3211b5b596917b75a3b
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-03 05:41:09 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const Discord = require("discord.js")
 
 module.exports = bot => {
     console.log(`${bot.user.username} is online`)
-    bot.user.setActivity("-help | twitch.tv/iflasheh", { type: "PLAYING", url: "" })
+    bot.user.setActivity("-help | twitch.tv/iflasheh", { type: "PLAYING", url: "https://www.twitch.tv/iflasheh" })
 }
