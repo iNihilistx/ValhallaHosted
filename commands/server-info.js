ccf910bfb0f9b6b52de498aba9e8d5525a4e5914
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-03 04:35:31 +0100

    Update server-info.js

--- commands/server-info.js
+++ commands/server-info.js
@@ -10,7 +10,7 @@ module.exports.run = async (bot, message, args) => {
         .setColor('#FFA500')
         .setTitle('Server Information: ')
         .setURL('https://i.imgur.com/G5bui5n.png')
-        .setAuthor(member.user.displayAvatar())
+        .setAuthor(member.user.displayAvatarURL())
         .setDescription(`Server information`)
         .setThumbnail('Valhalla', 'https://i.imgur.com/G5bui5n.png')
         .addFields(
