a7612f5d2905061f214feb9942256bf6e4c348bb
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-03 22:12:16 +0100

    Update discord-ban.js

--- commands/discord-ban.js
+++ commands/discord-ban.js
@@ -1,6 +1,6 @@
 module.exports = (client) => {
     const isInvite = async (guild, code) => {
-        return await new promise((resolve) => {
+        return await new Promise((resolve) => {
             guild.fetchInvites().then((invites) => {
                 for (const invite of invites) {
                     if (code === invite[0]) {
@@ -8,6 +8,7 @@ module.exports = (client) => {
                         return
                     }
                 }
+
                 resolve(false)
             })
         })
@@ -15,12 +16,16 @@ module.exports = (client) => {
 
     client.on('message', async (message) => {
         const { guild, member, content } = message
+
+        // discord.gg/23RAN4
+
         const code = content.split('discord.gg/')[1]
-        console.log('CODE: ', code)
+        console.log('CODE:', code)
 
         if (content.includes('discord.gg/')) {
             const isOurInvite = await isInvite(guild, code)
             if (!isOurInvite) {
+                // we know that they are advertising an outside discord server
             }
         }
     })
