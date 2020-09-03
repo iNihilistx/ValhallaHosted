53974516b9ebd75bbe98846b77e71e23f2ab8151
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-03 05:23:59 +0100

    Update server-info.js

--- commands/server-info.js
+++ commands/server-info.js
@@ -20,7 +20,7 @@ module.exports.run = async (bot, message, args) => {
             { name: 'Verification Level: ', value: `${message.guild.verificationLevel}`, inline: true }
         )
         .addField("Server Region: ", message.guild.region, true)
-        .addField("Roles: ", member.roles.map(roles => `${roles}`), join(', '), true)
+        .addField("Roles: ", message.member.roles, true)
         .setImage(message.guild.iconURL())
         .setTimestamp()
         .setFooter('Valhalla', 'https://i.imgur.com/G5bui5n.png')
