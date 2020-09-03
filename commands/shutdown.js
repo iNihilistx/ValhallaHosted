87ace45594be584e16cd0c6c40f1d3c7df87d3d2
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-03 21:56:40 +0100

    updates

---
+++ commands/shutdown.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js")
+const botconfig = require("../config.json");
+
+module.exports.run = async (bot, message, args) => {
+    if (message.author.id != "736834245498241093") return message.channel.send("You are the bot owner!")
+
+    try {
+        await message.channel.send("Valhallas connection has been terminated...")
+        process.exit()
+    } catch (e) {
+        message.channel.send(`ERROR: ${e.message}`)
+    }
+}
+
+module.exports.config = {
+    name: "shutdown",
+    description: "shuts down the bot",
+    usage: "-shutdown",
+    accessableby: "BOT OWNER",
+    aliases: ["botstop"]
+}
