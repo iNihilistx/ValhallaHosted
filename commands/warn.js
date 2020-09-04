88640912e6480e3f18cb18b4ccb7f31e6373a133
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-04 04:52:19 +0100

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,43 +1,43 @@
 const Discord = require('discord.js');
-
 const db = require('quick.db');
 
-module.exports = {
-    name: "warn",
-    description: "Warn a member",
+module.exports.run = async (bot, message, args) => {
+    if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("You lack the permissions needed for this command!");
+    const user = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
 
-    async run(client, message, args) {
-        if (!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send('You can\'t use that');
+    if (!user) return message.channel.send('Please specify a user, via mention or ID');
 
-        const user = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
+    if (user.bot) return message.channel.send('You can\'t warn bots');
 
-        if (!user) return message.channel.send('Please specify a user, via mention or ID');
+    if (message.author.id === user.id) return message.channel.send('You can\'t warn yourself nitwit');
 
-        if (user.bot) return message.channel.send('You can\'t warn bots');
+    if (message.guild.owner.id === user.id) return message.channel.send('You can\'t warn the server\'s owner');
 
-        if (message.author.id === user.id) return message.channel.send('You can\'t warn yourself nitwit');
+    let reason = args.slice(1).join(" ");
 
-        if (message.guild.owner.id === user.id) return message.channel.send('You can\'t warn the server\'s owner');
+    if (!reason) reason = 'Unspecified';
 
-        let reason = args.slice(1).join(" ");
+    let warnings = db.get(`warnings_${message.guild.id}_${user.id}`);
 
-        if (!reason) reason = 'Unspecified';
-
-        let warnings = db.get(`warnings_${message.guild.id}_${user.id}`);
-
-        if (warnings === 3) return message.channel.send(`${user} has already reached three warnings`);
+    if (warnings === 3) return message.channel.send(`${user} has already reached three warnings`);
 
 
-        if (warnings === null) {
-            db.set(`warnings_${message.guild.id}_${user.id}`, 1);
-            user.send(`You were warned in ${message.guild.name} for the follwoing reason: \`${reason}\``)
-            await message.channel.send(`**${user.username}** has been warned`)
-        }
-
-        if (warnings !== null) {
-            db.add(`warnings_${message.guild.id}_${user.id}`, 1)
-            user.send(`You were warned in ${message.guild.name} for the follwoing reason: \`${reason}\``)
-            await message.channel.send(`**${user.username}** has been warned`)
-        }
+    if (warnings === null) {
+        db.set(`warnings_${message.guild.id}_${user.id}`, 1);
+        user.send(`You were warned in ${message.guild.name} for the follwoing reason: \`${reason}\``)
+        await message.channel.send(`**${user.username}** has been warned`)
     }
+
+    if (warnings !== null) {
+        db.add(`warnings_${message.guild.id}_${user.id}`, 1)
+        user.send(`You were warned in ${message.guild.name} for the follwoing reason: \`${reason}\``)
+        await message.channel.send(`**${user.username}** has been warned`)
+    }
+}
+module.exports.config = {
+    name: "warn",
+    description: "",
+    usage: "-warn",
+    accessableby: "Moderators",
+    aliases: []
 }
