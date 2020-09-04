cf11b2d3a300c93ceedb592e3dcf5fd9eb068751
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-04 04:31:34 +0100

    sda

--- commands/report.js
+++ commands/report.js
@@ -1,50 +1,48 @@
-const { MessageEmbed } = require("discord.js");
+const warns = require("../models/warns");
 module.exports = {
-    name: "report",
-    description: "Report a user of your choice!",
-    usage: "<User mention>",
+    name: "warn",
+    description: "Warn a user",
+    category: "moderation",
+    usage: "<User mention> <Reason>",
     run: async (bot, message, args) => {
-        if (!message.member.permissions.has("MANAGE_MESSAGES"))
-            return message.channel.send(`No.`);
-        let User = message.mentions.users.first() || null;
-
-        if (User == null) {
-            return message.channel.send(`You did not mention a user!`);
-        } else {
-            let Reason = message.content.slice(bot.prefix.length + 22 + 7) || null;
-            if (Reason == null) {
-                return message.channel.send(
-                    `You did not specify a reason for the report!`
-                );
+        let user = message.mentions.users.first();
+        if (!user) return message.channel.send(`You did not mention a user!`);
+        if (!args.slice(1).join(" "))
+            return message.channel.send(`You did not specify a reason!`);
+        warns.findOne(
+            { Guild: message.guild.id, User: user.id },
+            async (err, data) => {
+                if (err) console.log(err);
+                if (!data) {
+                    let newWarns = new warns({
+                        User: user.id,
+                        Guild: message.guild.id,
+                        Warns: [
+                            {
+                                Moderator: message.author.id,
+                                Reason: args.slice(1).join(" "),
+                            },
+                        ],
+                    });
+                    newWarns.save();
+                    message.channel.send(
+                        `${user.tag} has been warned with the reason of ${args
+                            .slice(1)
+                            .join(" ")}. They now have 1 warn.`
+                    );
+                } else {
+                    data.Warns.unshift({
+                        Moderator: message.author.id,
+                        Reason: args.slice(1).join(" "),
+                    });
+                    data.save();
+                    message.channel.send(
+                        `${user.tag} has been warned with the reason of ${args
+                            .slice(1)
+                            .join(" ")}. They know have ${data.Warns.length} warns.`
+                    );
+                }
             }
-            let Avatar = User.displayAvatarURL();
-            let Channel = message.guild.channels.cache.find(
-                (ch) => ch.name === "reports"
-            );
-            if (!Channel)
-                return message.channel.send(
-                    `There is no channel in this guild which is called \`reports\``
-                );
-            let Embed = new MessageEmbed()
-                .setTitle(`New report!`)
-                .setDescription(
-                    `The moderator \`${message.author.tag}\` has reported the user \`${User.tag}\`! `
-                )
-                .setColor(`RED`)
-                .setThumbnail(Avatar)
-                .addFields(
-                    { name: "Mod ID", value: `${message.author.id}`, inline: true },
-                    { name: "Mod Tag", value: `${message.author.tag}`, inline: true },
-                    { name: "Reported ID", value: `${User.id}`, inline: true },
-                    { name: "Reported Tag", value: `${User.tag}`, inline: true },
-                    { name: "Reason", value: `\`${Reason.slice(1)}\``, inline: true },
-                    {
-                        name: "Date (M/D/Y)",
-                        value: `${new Intl.DateTimeFormat("en-US").format(Date.now())}`,
-                        inline: true,
-                    }
-                );
-            Channel.send(Embed);
-        }
+        );
     },
 };
