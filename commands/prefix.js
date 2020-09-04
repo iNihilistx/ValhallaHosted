3f21c378d221b4d1ac9a5b39085076ebde263253
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-04 05:11:42 +0100

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,18 +1,20 @@
-const Discord = require("discord.js");
 const db = require('quick.db');
 
-module.exports.run = async (bot, message, args) => {
-    if (!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send('You lack the permissions needed for this command!');
-    if (!args[0]) return message.channel.send("Please provide a new prefix");
-    if (args[1]) return message.channel.send("The Prefix cannot have two spaces!");
-    db.set(`prefix_${message.guild.id}`, args[0])
-    message.channel.send(`The prefix has successfully been changed to **${args[0]}**`)
-}
-
-module.exports.config = {
-    name: "prefix",
+module.exports = {
+    name: "setprefix",
     usage: "-prefix",
-    description: "changes the prefix",
-    accessableby: "MANAGE_SERVER",
-    aliases: []
+    description: "Set a server's prefix",
+    aliases: [],
+
+    async run(client, message, args) {
+        if (!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send('You don\'t have permission to use that.');
+
+        if (!args[0]) return message.channel.send('Please provide a new prefix');
+
+        if (args[1]) return message.channel.send('The prefix can\'t have two spaces');
+
+        db.set(`prefix_${message.guild.id}`, args[0])
+
+        message.channel.send(`Succesffully set new prefix to **${args[0]}**`)
+    }
 }
