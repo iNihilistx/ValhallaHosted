ccad3596c6ee02bf3ce2e71912b795eea4bcde2d
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-04 22:38:39 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const Discord = require("discord.js")
 
 module.exports = bot => {
     console.log(`${bot.user.username} is online`)
-    bot.user.setActivity("-help | Nairobi", { type: "PLAYING", url: "Nairobi" })
+    bot.user.setActivity("-help | Nairobi", { type: "PLAYING", url: "https://github.com/Naiirobii" })
 }
