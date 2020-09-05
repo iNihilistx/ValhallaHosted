94a4d06cc7b5682b68a3de68b491b8a706b7d70a
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-05 05:43:10 +0100

    sd

--- events/ready.js
+++ events/ready.js
@@ -1,27 +1,17 @@
-const Event = require('../event');
+const Discord = require("discord.js")
+const botconfig = require("../config.json")
 
-module.exports = class extends Event {
+module.exports = bot => {
+    console.log(`${bot.user.username} is now online`)
 
-    constructor(...args) {
-        super(...args, {
-            once: true
-        })
-    }
+    let statuses = [
+        `${bot.guilds.cache.size}Servers!`,
+        "-help",
+        `over ${bot.users.size} users!`
+    ]
 
-    run() {
-        console.log([
-            `Logged in as ${this.client.user.tag}`,
-            `Loaded ${this.client.commands.size} commands!`,
-            `Loaded ${this.client.events.size} events`
-        ].join('\n'));
-
-        const activities = [
-            `${this.client.guilds.cache.size} servers`,
-            `${this.client.channels.cache.size} channels`,
-            `${this.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)} users`
-        ];
-
-        let i = 0;
-        setInterval(() => this.client.user.setActivity(`${this.client.prefix}help | ${activities[i++ % activities.length]}`, { type: "WATCHING" }), 15000);
-    }
+    setInterval(function () {
+        let status = statuses[Math.floor(Math.random() * statuses.length)];
+        bot.user.setActivity(status, { type: "WATCHING" });
+    }, 5000)
 }
