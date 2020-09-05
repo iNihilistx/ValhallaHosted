1e37414cf6846c78c784711dfac56a116a1882c2
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-05 04:36:45 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,20 @@
 const Discord = require("discord.js")
 
+
 module.exports = bot => {
     console.log(`${bot.user.username} is online`)
-    let activities = [`${bot.guilds.size} servers`, `${bot.channels.size} channels`, `${bot.users.size}users`], i = 0;
-    setInterval(() => bot.user.setActivity(`${bot.prefix}help | ${activities[i++ % activities.length]}`, { type: "WATCHING " }), 15000)
+    // bot.user.setActivity("Hello", {type: "STREAMING", url:"https://twitch.tv/Strandable"});
+
+    let statuses = [
+        `${bot.guilds.size} servers!`,
+        "!help",
+        `over ${bot.users.size} users!`
+    ]
+
+    setInterval(function () {
+        let status = statuses[Math.floor(Math.random() * statuses.length)];
+        bot.user.setActivity(status, { type: "WATCHING" });
+
+    }, 5000)
+
 }
