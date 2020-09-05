247a1c11fab1a577bcbd8eb2c0c87626d316359e
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-05 18:50:58 +0100

    Update purpose.js

--- commands/purpose.js
+++ commands/purpose.js
@@ -5,7 +5,6 @@ module.exports.run = async (bot, message, args) => {
     const purposeEmbed = new Discord.MessageEmbed()
         .setImage('https://i.imgur.com/sdNIc8t.gif')
         .setTitle('What is my purpose?')
-        .setURL('https://i.imgur.com/sdNIc8t.gif')
 
     message.channel.send(purposeEmbed);
 }
