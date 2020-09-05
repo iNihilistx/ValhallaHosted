c803265bfb36b9c99d994d1cefb2d4f27107a8ed
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-05 06:13:26 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -7,7 +7,7 @@ module.exports = bot => {
     let statuses = [
         `${bot.guilds.cache.size} Servers!`,
         "=help",
-        "Being Developed"
+        "Nairobi develop me"
     ]
 
     setInterval(function () {
