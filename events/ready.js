317997fdf26ccc690d5957f42e30b9894eedc14f
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-05 04:33:25 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ const Discord = require("discord.js")
 
 module.exports = bot => {
     console.log(`${bot.user.username} is online`)
-    let activities = [`${bot.guilds.size} servers`, `${bot.channels.size} channels`, `${bot.users.size}`], i = 0;
+    let activities = [`${bot.guilds.size} servers`, `${bot.channels.size} channels`, `${bot.users.size}users`], i = 0;
     setInterval(() => bot.user.setActivity(`${bot.prefix}help | ${activities[i++ % activities.length]}`, { type: "WATCHING " }), 15000)
 }
