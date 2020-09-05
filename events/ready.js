622250f0f56b5191f97ac137244d1585a69a596b
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-05 04:38:51 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,20 +1,17 @@
 const Discord = require("discord.js")
 
-
 module.exports = bot => {
-    console.log(`${bot.user.username} is online`)
-    // bot.user.setActivity("Hello", {type: "STREAMING", url:"https://twitch.tv/Strandable"});
+    const activities_list = [
+        "with the &help command.",
+        "with the developers console",
+        "with some code",
+        "with JavaScript"
+    ]; // creates an arraylist containing phrases you want your bot to switch through.
 
-    let statuses = [
-        `${bot.guilds.size} servers!`,
-        "!help",
-        `over ${bot.users.size} users!`
-    ]
-
-    setInterval(function () {
-        let status = statuses[Math.floor(Math.random() * statuses.length)];
-        bot.user.setActivity(status, { type: "WATCHING" });
-
-    }, 5000)
-
+    bot.on('ready', () => {
+        setInterval(() => {
+            const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
+            bot.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+        }, 10000); // Runs this every 10 seconds.
+    });
 }
