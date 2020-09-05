c4292f09705aa42004af764a7ee5d03483154952
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-05 23:34:21 +0100

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,30 +1,30 @@
-var Discord = require('discord.js');
+const Discord = require('discord.js');
 
-exports.run = async (client, msg, args) => {
+exports.run = async (client, message, args) => {
     if (!msg.member.hasPermission('MANAGE_MESSAGES')) return msg.reply('You can\'t use that!');
 
-    var user = msg.mentions.users.first();
-    if (!user) return msg.reply('You didn\'t mention anyone!');
+    var user = message.mentions.users.first();
+    if (!user) return message.reply('You didn\'t mention anyone!');
 
     var member;
 
     try {
-        member = await msg.guild.members.fetch(user);
+        member = await message.guild.members.fetch(user);
     } catch (err) {
         member = null;
     }
 
-    if (!member) return msg.reply('They aren\'t in the server!');
+    if (!member) return message.reply('They aren\'t in the server!');
 
     var reason = args.splice(1).join(' ');
-    if (!reason) return msg.reply('You need to give a reason!');
+    if (!reason) return message.reply('You need to give a reason!');
 
-    var channel = msg.guild.channels.cache.find(c => c.name === 'warn');
+    var channel = message.guild.channels.cache.find(c => c.name === 'warn');
 
     var log = new Discord.MessageEmbed()
         .setTitle('User Warned')
         .addField('User:', user, true)
-        .addField('By:', msg.author, true)
+        .addField('By:', message.author, true)
         .addField('Reason:', reason)
     channel.send(log);
 
@@ -38,5 +38,5 @@ exports.run = async (client, msg, args) => {
         console.warn(err);
     }
 
-    msg.channel.send(`**${user}** has been warned by **${msg.author}**!`);
+    message.channel.send(`**${user}** has been warned by **${message.author}**!`);
 }
