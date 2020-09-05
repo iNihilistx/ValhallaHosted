06fd4fc68718528ca232fb8b91dd4a4131e45198
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-05 18:41:07 +0100

    Update purpose.js

--- commands/purpose.js
+++ commands/purpose.js
@@ -1,29 +1,37 @@
+
 const Discord = require("discord.js")
 const botconfig = require("../config.json");
 
 module.exports.run = async (bot, message, args) => {
     let userArray = message.content.split(" ");
     let userArgs = userArray.slice(1);
-    let member = message.mentions.members.first() || message.guild.members.cache.get(Args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join("") || x.user.username === userArgs[(0)]) || message.member;
+    let member = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join("") || x.user.username === userArgs[0]) || message.member;
 
-    const mypurposeEmbed = new Discord.MessageEmbed()
+    const purposeEmbed = new Discord.MessageEmbed()
         .setColor('#FFA500')
-        .setTitle('What is my purpose?')
+        .setTitle('Server Information: ')
         .setURL('https://i.imgur.com/sdNIc8t.gif')
         .setAuthor(member.user.tag, member.user.displayAvatarURL())
-        .setDescription(`You pass butter`)
+        .setDescription(`All the information for this guild`)
         .setThumbnail('https://i.imgur.com/G5bui5n.png')
+        .addFields(
+            { name: "Server Name: ", value: `${message.guild.name}` },
+            { name: "Current Members: ", value: `${message.guild.memberCount}` },
+            { name: 'Server Owner: ', value: `${message.guild.owner}`, inline: true },
+            { name: 'Verification Level: ', value: `${message.guild.verificationLevel}`, inline: true }
+        )
+        .addField("Server Region: ", message.guild.region, true)
+        .setImage(message.guild.iconURL())
         .setTimestamp()
-        .setFooter('vallhalla', 'https://i.imgur.com/G5bui5n.png')
+        .setFooter('Valhalla', 'https://i.imgur.com/G5bui5n.png')
 
-    message.channel.send(mypurposeEmbed);
+    message.channel.send(purposeEmbed);
 }
 
 module.exports.config = {
-    name: "Purpose",
-    setDescription: "bot gif",
-    usage: "=purpose",
+    name: "server",
+    description: "shows server information",
+    usage: "-purpose",
     accessableby: "Members",
-    aliases: []
-
+    aliases: [""]
 }
