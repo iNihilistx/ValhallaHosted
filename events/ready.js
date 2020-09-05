c59f7b2717433a05f8d948f8fca706d3ca3bb438
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-05 05:03:13 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,22 +1,17 @@
 const Discord = require("discord.js")
 const botconfig = require("../config.json")
 
-let userArray = message.content.split(" ");
-let userArgs = userArray.slice(1);
-let member = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join("") || x.user.username === userArgs[0]) || message.member;
-
 module.exports = bot => {
     console.log(`${bot.user.username}is now online`)
 
     let statuses = [
         `${bot.guilds.size}Servers!`,
         "-help",
-        `over ${message.guild.memberCount} users!`
+        `over ${bot.users.size} users!`
     ]
+
     setInterval(function () {
         let status = statuses[Math.floor(Math.random() * statuses.length)];
         bot.user.setActivity(status, { type: "WATCHING" });
-
     }, 5000)
-
 }
