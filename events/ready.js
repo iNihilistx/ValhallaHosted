e2eb51d294f2e8a2a368328360c57b9450f6a24d
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-05 05:01:54 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,10 @@
 const Discord = require("discord.js")
 const botconfig = require("../config.json")
 
+let userArray = message.content.split(" ");
+let userArgs = userArray.slice(1);
+let member = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join("") || x.user.username === userArgs[0]) || message.member;
+
 module.exports = bot => {
     console.log(`${bot.user.username}is now online`)
 
