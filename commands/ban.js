5ff192656de4d6a7ec87e4e475c450f5d45897e2
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-07 02:33:37 +0100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -3,7 +3,7 @@ module.exports.run = async (bot, message, args) => {
 		message.channel.send("You lack the permissions needed for this command!");
 	else {
 		try {
-			let bannedMember = await message.guild.members.ban(args);
+			let bannedMember = message.mentions.members.first(args);
 			if (bannedMember)
 				console.log(bannedMember.tag + " was banned from the server!");
 			message.channel.send(`${bannedMember} has been banned from the server!`)
