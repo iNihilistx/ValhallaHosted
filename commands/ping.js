14df22d4337de1d15c453ce6b4fcefd6f8e9ed76
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-10 18:48:38 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const botconfig = require('../config.json');
 
 module.exports.run = async (bot, message, args) => {
     const m = await message.channel.send("pinging...");
-    m.edit(`Recieved! Latency is ${m.createdTimeStamp - message.createdTimeStamp}ms. API Latency is${Math.round(client.ping)}ms `);
+    m.edit(`Recieved! Latency is ${m.createdTimeStamp - message.createdTimeStamp}ms. API Latency is${Math.round(bot.ping)}ms `);
 
 }
 
