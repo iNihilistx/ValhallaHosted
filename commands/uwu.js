fc22d403c627d16651838bd9cc7431ba448a4a8d
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-10 17:04:51 +0100

    Update uwu.js

--- commands/uwu.js
+++ commands/uwu.js
@@ -3,7 +3,7 @@ const Botconfig = require('../config.json');
 
 module.exports.run = async (bot, message, args) => {
 
-    const images = ["https://i.imgur.com/NpYpXgI.gif", "https://i.imgur.com/jJ2N5Zu.gif"];
+    const images = ["https://i.imgur.com/NpYpXgI.gif", "https://i.imgur.com/jJ2N5Zu.gif", "https://i.imgur/K6bWrmL.gif"];
     const image = images[Math.floor(Math.random() * images.length)];
     const uwuEmbed = new Discord.MessageEmbed()
         .setImage(image)
