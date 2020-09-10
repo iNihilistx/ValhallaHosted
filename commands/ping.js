73b7fbd487bf85f5873ebe3016f71238baa751ae
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-10 18:47:35 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,13 +2,8 @@ const Discord = require('discord.js')
 const botconfig = require('../config.json');
 
 module.exports.run = async (bot, message, args) => {
-    const msg = await message.channel.send('Pinging...');
-
-    const latency = msg.createdTimestamp - message.createdTimestamp;
-    const choices = ['Is this really my ping?', 'Is this okay? I can\'t look!', 'I hope it isn\'t bad!'];
-    const response = choices[Math.floor(Math.random() * choices.length)];
-
-    msg.edit(`${response} - Bot Latency: \`${latency}ms\`, API Latency: \`${Math.round(this.client.ws.ping)}ms\``);
+    const m = await message.channel.send("pinging...");
+    m.edit(`Recieved! Latency is ${m.createdTimeStamp - message.createdTimeStamp}ms. API Latency is${Math.round(client.ping)}ms `);
 
 }
 
