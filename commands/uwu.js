faf7e670666b1dded6fe5831fd75140945af8e2c
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-10 17:01:42 +0100

    Update uwu.js

--- commands/uwu.js
+++ commands/uwu.js
@@ -6,7 +6,6 @@ module.exports.run = async (bot, message, args) => {
     const images = ["https://i.imgur.com/NpYpXgI.gif", "https://i.imgur.com/jJ2N5Zu.gif"];
     const image = images[Math.floor(Math.random() * images.length)];
     const uwuEmbed = new Discord.MessageEmbed()
-        .setColor("#FFA500")
         .setImage(image)
 
     message.channel.send(uwuEmbed);
