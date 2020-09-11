93d2709c921906cd861fbe662b5ed0c88c7f82cd
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-11 21:07:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ bot.on("guildMemberAdd", member => {
         .setTitle("Welcome User")
         .setURL('https://i.imgur.com/G5bui5n.png')
         .setAuthor(member.user.tag, member.user.displayAvatarURL())
-        .addfield(welcomeChannel.send(`${member} has joined the cult`))
+        .addField(welcomeChannel.send(`${member} has joined the cult`))
 
     message.channel.send(welcomeEmbed);
 })
