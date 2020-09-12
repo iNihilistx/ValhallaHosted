94134ca0bb38afca4588e235005a0397c94354f7
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-12 22:08:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const bot = new Discord.Client({ disableEveryone: true });
 bot.on("guildMemberAdd", member => {
     const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'welcome')
 
+    const role = guild.roles.find(role => role.name === "dudes");
+    member.addRole(role)
+
     welcomeChannel.send(`${member} has joined the cult`)
 })
 
-bot.on("serverNewMember", function (server, user) {
-    mybot.addMemberToRole(user, server.roles.get("name", "dudes"), function (err) { if (err) console.log(err) })
-})
 bot.on("guildMemberRemove", member => {
     const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === "goodbye")
     welcomeChannel.send(` ${member} has left the cult`)
