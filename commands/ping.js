eef52207d383eb802a842465e87354a3f12322bf
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-12 04:09:44 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,12 +5,13 @@ module.exports.run = async (bot, message, args) => {
     var yourping = new Date().getTime() - message.createdTimeStamp
     var botping = Math.round(bot.ws.ping)
 
-    message.channel.send("Pinging...").then(m => {
+    message.channel.send("The Current Ping...").then(m => {
         var ping = m.createdTimeStamp - message.createdTimeStamp;
 
         var ping = new Discord.MessageEmbed()
-            .setAuthor(`Your ping is: ${ping} \n The bots ping is: ${botping}ms`)
+            .setAuthor(` Valhallas ping is: ${botping}ms`)
             .setColor(`#FFA500`)
+            .setTimestamp()
 
         m.edit(ping)
     });
