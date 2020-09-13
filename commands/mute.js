eb608a6a127095a8b3df329baaace2768ee4b639
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-13 01:45:23 +0100

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -1,22 +1,48 @@
-module.exports.run = async (bot, message, args) => {
-    if (!message.member.hasPermission(['MUTE_MEMBERS'])) return;
-    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" ") || x.user.username === args[0])
-    if (member.hasPermission(['KICK_MEMBERS', 'BAN_MEMBERS']) && !message.member.hasPermission('MODERATOR')) return;
-
-    let mutedRole = message.guild.roles.cache.get('mute');
-    let verifiedRole = message.guild.roles.cache.get('737386427637170239)');
-    if (mutedRole) {
-        member.roles.add(mutedRole);
-        member.roles.remove(verifiedRole);
-        message.channel.send("User has been muted.");
+const { MessageEmbed } = require("discord.js")
+const Botconfig = require('../ config.json')
+module.exports.run = async (message, bot, args) => {
+    if (!message.member.hasPermission("MANAGE_ROLES")) {
+        return message.channel.send(
+            "You lack the needed permissions"
+        );
     }
+
+    if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
+        return message.channel.send("I lack the needed permissions");
+    }
+
+    const user = message.mentions.member.first();
+
+    if (!user) {
+        return message.channel.send("Can't use this to mute yourself dumbass");
+    }
+
+    let reason = args.slice(1).join(" ")
+
+    if (!reason) {
+        return message.channel.send("please provide a reason for the mute");
+
+    }
+
+    let muterole = message.guild.cache.find(x => x.name === "mute")
+
+    if (!muterole) {
+        return message.channel.send("The server lacks the needed role for this action")
+    }
+
+    if (user.roles.cache.has(muterole)) {
+        return message.channel.send("User already has the role")
+    }
+
+    user.roles.add(muterole)
+
+    await message.channel.send(`You have muted ${message.mentions.users.first().username} for \`${reason}\``)
+    user.send(`You have been muted in ${message.guild.name} for \`${reason}\``)
 }
 
 module.exports.config = {
     name: "mute",
-    description: "",
     usage: "=mute",
-    accessableby: "Moderators",
+    description: "mutes user",
     aliases: []
 }
-
