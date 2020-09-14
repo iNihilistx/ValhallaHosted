630c9fd96eae33a2e79c12cacd69169979127bc9
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-14 06:17:04 +0100

    Update member-count.js

--- commands/member-count.js
+++ commands/member-count.js
@@ -1,8 +1,8 @@
 module.exports.run = async (client, message, arguments) => {
-    const guild = client.guilds.get("566596189827629066");
+    const guild = client.guilds.get("737386427637170239");
     setInterval(function () {
         var memberCount = guild.members.filter(member => !member.user.bot).size;
-        var memberCountChannel = client.channels.get("626462657817477131");
+        var memberCountChannel = client.channels.get("754931819064852510");
         memberCountChannel.setName(`${guild.name} has ${memberCount} members!`);
     }, 1000);
 };
