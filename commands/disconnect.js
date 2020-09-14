e7f4b3daf2e0d8983e7f928cbef03f991f7537e3
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-14 07:35:48 +0100

    Update disconnect.js

--- commands/disconnect.js
+++ commands/disconnect.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
     if (!message.guild.me.hasPermission('MOVE_MEMBERS')) return message.reply('Missing the required `Move Members` permission.');
 
     const member = message.mentions.members.first();
-    if (!member) return message.reply('You need to @mention a user/bot to kick from the voice channel.');
+    if (!member) return message.reply('You need to @mention a user in order to kick them from the voice channel.');
     if (!member.voiceChannel) return message.reply('That user isn\'t in a voice channel.');
 
     member.member.voice.kick();
