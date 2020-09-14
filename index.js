2a98bb1a50d1dd864ac2dbaab4e33f073c594340
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-14 23:22:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,28 @@ bot.on("guildMemberRemove", member => {
     welcomeChannel.send(` ${member} has left the cult`)
 })
 
+bot.on("message", async message => {
+    if (message.author.bot || message.channel.type === "dm") return;
+
+    const messageArray = message.content.split(' ');
+    const cmd = messageArray[0];
+    const args = messageArray.slice(1);
+
+    if (cmd === '??poll') {
+        let pollChannel = message.mentions.channels.first();
+        let pollDescription = args.slice(1).join(' ');
+
+        let embedPoll = new Discord.MessageEmbed()
+            .setTitle(' New Poll!')
+            .setDescription(pollDescription)
+            .setColor('ORANGE')
+        let msgEmbed = await pollChannel.send(embedPoll);
+        await msgEmbed.react('👍')
+        await msgEmbed.react('👎')
+    }
+
+})
+
 require("./util/eventHandler")(bot)
 
 const fs = require("fs");
