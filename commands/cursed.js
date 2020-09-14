2753fc60eb1a2252e0c8f480b27b01cc5a30505e
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-14 18:46:50 +0100

    Update cursed.js

--- commands/cursed.js
+++ commands/cursed.js
@@ -1,3 +1,4 @@
+const usedCommand = new Set();
 const Discord = require('discord.js');
 const randomPuppy = require('random-puppy');
 
@@ -6,14 +7,26 @@ module.exports.run = async (bot, message, args) => {
     const random = subReddits[Math.floor(Math.random() * subReddits.length)];
     const img = await randomPuppy(random);
 
-    const embed = new Discord.MessageEmbed()
-        .setImage(img)
-        .setTitle(`From /r/${random}`)
-        .setURL(`http://reddit.com/${random}`)
-        .setTimestamp()
-        .setFooter('Valhalla', 'https://i.imgur.com/G5bui5n.png')
+    if (usedCommand.has(message.author.id)) { // << this means that if the command has been used it'll display a message saying that you're unable to use the command
+        message.reply('You are currently in a cooldown. Wait 15 seconds and try again...')
+    } else {
 
-    message.channel.send(embed);
+        // the else is where the code is then written to then run the command
+
+        const embed = new Discord.MessageEmbed()
+            .setImage(img)
+            .setTitle(`From /r/${random}`)
+            .setURL(`http://reddit.com/${random}`)
+            .setTimestamp()
+            .setFooter('Valhalla', 'https://i.imgur.com/G5bui5n.png')
+
+        message.channel.send(embed);
+
+        usedCommand.add(message.author.id); // this will then delete the message after 15 seconds
+        setTimeout(() => {
+            usedCommand.delete(message.author.id);
+        }, 15000); // this is set within Miliseconds
+    }
 
 }
 
