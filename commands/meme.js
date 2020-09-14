d8bc3e6109a0e05f126e4fe59250d35d60b37590
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-14 06:57:31 +0100

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -1,36 +1,35 @@
-
-const usedCommand = new Set();
+const talkedRecently = new Set()
+const Discord = require('discord.js');
+const randomPuppy = require('random-puppy');
 
 module.exports.run = async (bot, message, args) => {
     const subReddits = ["meme", "me_irl", "dankmeme", "gamingmemes", "memes"]
     const random = subReddits[Math.floor(Math.random() * subReddits.length)];
     const img = await randomPuppy(random);
 
-    if (usedCommand.has(message.author.id)) {
-        message.reply('You cannot use the command beacuse of the cooldown.')
-    } else {
+    const embed = new Discord.MessageEmbed()
+        .setImage(img)
+        .setTitle(`From /r/${random}`)
+        .setURL(`http://reddit.com/${random}`)
+        .setTimestamp()
+        .setFooter('Valhalla', 'https://i.imgur.com/G5bui5n.png')
 
-        const embed = new Discord.MessageEmbed()
-            .setImage(img)
-            .setTitle(`From /r/${random}`)
-            .setURL(`http://reddit.com/${random}`)
-            .setTimestamp()
-            .setFooter('Valhalla', 'https://i.imgur.com/G5bui5n.png')
+    message.channel.send(embed);
 
-        message.channel.send(embed);
+    if (talkedRecently.has(message.author.id))
+        return;
 
-    }
-
-    usedCommand.add(message.author.id);
+    talkedRecently.add(message.author.id);
     setTimeout(() => {
-        usedCommand.delete(message.author.id);
-    }, 5000); //You can set the ammount of the cooldown here! Its Formated to Miliseconds.
+        talkedRecently.delete(message.author.id);
+    }, 5000);
+
 }
 
 module.exports.config = {
-    name: "cooldown",
-    description: "meme with cooldown.",
-    usage: "??cooldown",
+    name: "meme",
+    description: "",
+    usage: "=meme",
     accessableby: "Members",
     aliases: []
 }
