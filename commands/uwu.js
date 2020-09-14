db4833339ed3197500cbb4e6fa8164e9eb67add5
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-14 18:37:20 +0100

    Update uwu.js

--- commands/uwu.js
+++ commands/uwu.js
@@ -1,3 +1,4 @@
+const usedCommand = new Set();
 const Discord = require('discord.js');
 const botconfig = require('../config.json');
 
@@ -5,17 +6,27 @@ module.exports.run = async (bot, message, args) => {
 
     const images = ["https://i.imgur.com/1dDkQNL.gif", "https://i.imgur.com/rjTA8fC.gif", "https://i.imgur.com/Pg4Kp2q.gif", "https://i.imgur.com/NpYpXgI.gif", "https://i.imgur.com/jJ2N5Zu.gif", "https://i.imgur.com/K6bWrmL.gif"];
     const image = images[Math.floor(Math.random() * images.length)];
-    const uwuEmbed = new Discord.MessageEmbed()
-        .setColor("#FFA500")
-        .setTitle('UWU')
-        .setURL('https://i.imgur.com/G5bui5n.png')
-        .setImage(image)
-        .setTimestamp()
-        .setFooter("Valhalla", 'https://i.imgur.com/G5bui5n.png')
+
+    if (usedCommand.has(message.author.id)) {
+        message.reply('You are currently in a cooldown. Wait 5 seconds and try again...')
+    } else {
+        const uwuEmbed = new Discord.MessageEmbed()
+            .setColor("#FFA500")
+            .setTitle('UWU')
+            .setURL('https://i.imgur.com/G5bui5n.png')
+            .setImage(image)
+            .setTimestamp()
+            .setFooter("Valhalla", 'https://i.imgur.com/G5bui5n.png')
 
 
 
-    message.channel.send(uwuEmbed);
+        message.channel.send(uwuEmbed);
+
+        usedCommand.add(message.author.id);
+        setTimeout(() => {
+            usedCommand.delete(message.author.id);
+        }, 5000);
+    }
 }
 
 module.exports.config = {
