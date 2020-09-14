ea3c2bfb1a852773c5f89d1e1610a3615edfddb2
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-14 07:05:26 +0100

    Update disconnect.js

--- commands/disconnect.js
+++ commands/disconnect.js
@@ -2,15 +2,19 @@ const Discord = require('discord.js')
 const botconfig = require('../config.json')
 
 module.exports.run = async (bot, message, args) => {
-    if (!message.guild.me.hasPermission('MOVE_MEMBERS')) return message.reply('Missing the needed permissions');
+    // Make sure the bot user has permissions to move members in the guild:
+    if (!message.guild.me.hasPermission('MOVE_MEMBERS')) return message.reply('Missing the required `Move Members` permission.');
 
+    // Get the mentioned user/bot and check if they're in a voice channel:
     const member = message.mentions.members.first();
-    if (!member) return message.reply('You need to @mention a user to disconnect from the voice channel');
-    if (!member.voiceChannel) return message.reply('That user isn\'t in a voice channel');
+    if (!member) return message.reply('You need to @mention a user/bot to kick from the voice channel.');
+    if (!member.voiceChannel) return message.reply('That user/bot isn\'t in a voice channel.');
 
+    // Now we set the member's voice channel to null, in other words disconnecting them from the voice channel.
     member.setVoiceChannel(null);
 
-    message.react('👌')
+    // Finally, pass some user response to show it all worked out:
+    message.react('👌');
 }
 
 module.exports.config = {
