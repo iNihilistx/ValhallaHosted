e142b0148f36207cc6319638a350547a8782751c
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-14 20:52:26 +0100

    Update purpose.js

--- commands/purpose.js
+++ commands/purpose.js
@@ -1,29 +1,18 @@
-const usedCommand = new Set();
 const Discord = require('discord.js');
 const Botconfig = require('../config.json');
 
 module.exports.run = async (bot, message, args) => {
+    const purposeEmbed = new Discord.MessageEmbed()
+        .setColor("#FFA500")
+        .setImage('https://i.imgur.com/sdNIc8t.gif')
+        .setTitle('What is my purpose')
+        .setDescription('You pass butter')
+        .setTimestamp()
+        .setFooter('Valhalla', 'https://i.imgur.com/G5bui5n.png')
 
-    if (usedCommand.has(message.author.id)) {
-        message.reply("You are currently in a cooldown. Wait 15 seconds and try again...").then(m => m.delete({ timeout: 5000 }))
-    } else {
-
-        const purposeEmbed = new Discord.MessageEmbed()
-            .setColor("#FFA500")
-            .setImage('https://i.imgur.com/sdNIc8t.gif')
-            .setTitle('What is my purpose')
-            .setDescription('You pass butter')
-            .setTimestamp()
-            .setFooter('Valhalla', 'https://i.imgur.com/G5bui5n.png')
-
-        message.channel.send(purposeEmbed);
-
-        usedCommand.add(message.author.id);
-        setTimeout(() => {
-            usedCommand.delete(message.author.id);
-        }, 15000);
-    }
+    message.channel.send(purposeEmbed);
 }
+
 module.exports.config = {
     name: "purpose",
     description: "",
