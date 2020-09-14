fd37bf5509166b18ea81ebea2041c403237b412a
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-14 18:04:04 +0100

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -6,31 +6,11 @@ module.exports.run = async (bot, message, args) => {
     if (usedCommand.has(message.author.id)) {
         message.reply('This command is currently in cooldown')
     } else {
-
-        const subReddits = ["meme", "me_irl", "dankmeme", "gamingmemes", "memes"]
-        const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-        const img = await randomPuppy(random);
-
-        const embed = new Discord.MessageEmbed()
-            .setImage(img)
-            .setTitle(`From /r/${random}`)
-            .setURL(`http://reddit.com/${random}`)
-            .setTimestamp()
-            .setFooter('Valhalla', 'https://i.imgur.com/G5bui5n.png')
-
-        message.channel.send(embed);
+        message.reply('cooldown is off')
 
         usedCommand.add(message.author.id);
         setTimeout(() => {
 
-        }, 3000);
+        }, timeout);
     }
-}
-
-module.exports.config - {
-    name: "meme",
-    usage: "??meme",
-    description: "Sends a meme",
-    accessableby: "Members",
-    aliases: []
 }
