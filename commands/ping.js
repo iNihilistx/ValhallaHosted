1adc13972a140d46d69a1b470b29326923bd5f60
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-14 18:53:27 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,21 +1,32 @@
+const usedCommand = new Set();
 const Discord = require('discord.js')
 const botconfig = require('../config.json');
 
 module.exports.run = async (bot, message, args) => {
-    var yourping = new Date().getTime() - message.createdTimeStamp
-    var botping = Math.round(bot.ws.ping)
+    if (usedCommand.has(message.author.id)) {
+        message.reply('You are currently in a cooldown. Wait 30 seconds and try again...')
+    } else {
 
-    message.channel.send("Checking...").then(m => {
-        var ping = m.createdTimeStamp - message.createdTimeStamp;
+        var yourping = new Date().getTime() - message.createdTimeStamp
+        var botping = Math.round(bot.ws.ping)
 
-        var ping = new Discord.MessageEmbed()
-            .setAuthor(` The Ping For Valhalla is: ${botping}ms`)
-            .setColor(`#FFA500`)
-            .setTimestamp()
-            .setFooter('Valhalla', 'https://i.imgur.com/G5bui5n.png')
+        message.channel.send("Checking...").then(m => {
+            var ping = m.createdTimeStamp - message.createdTimeStamp;
 
-        m.edit(ping)
-    });
+            var ping = new Discord.MessageEmbed()
+                .setAuthor(` The Ping For Valhalla is: ${botping}ms`)
+                .setColor(`#FFA500`)
+                .setTimestamp()
+                .setFooter('Valhalla', 'https://i.imgur.com/G5bui5n.png')
+
+            m.edit(ping)
+        });
+
+        usedCommand.add(message.author.id);
+        setTimeout(() => {
+            usedCommand.delete(message.author.id);
+        }, 30000);
+    }
 
 }
 
