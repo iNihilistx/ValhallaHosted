b2d8ab193f95b71d7de1f4e441e1bd6d5c356566
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-14 18:02:10 +0100

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -1,26 +1,36 @@
 const Discord = require('discord.js');
 const randomPuppy = require('random-puppy');
+const usedCommand = new Set()
 
 module.exports.run = async (bot, message, args) => {
-    const subReddits = ["meme", "me_irl", "dankmeme", "gamingmemes", "memes"]
-    const random = subReddits[Math.floor(Math.random() * subReddits.length)];
-    const img = await randomPuppy(random);
+    if (usedCommand.has(message.author.id)) {
+        message.reply('This command is currently in cooldown')
+    } else {
 
-    const embed = new Discord.MessageEmbed()
-        .setImage(img)
-        .setTitle(`From /r/${random}`)
-        .setURL(`http://reddit.com/${random}`)
-        .setTimestamp()
-        .setFooter('Valhalla', 'https://i.imgur.com/G5bui5n.png')
+        const subReddits = ["meme", "me_irl", "dankmeme", "gamingmemes", "memes"]
+        const random = subReddits[Math.floor(Math.random() * subReddits.length)];
+        const img = await randomPuppy(random);
 
-    message.channel.send(embed);
+        const embed = new Discord.MessageEmbed()
+            .setImage(img)
+            .setTitle(`From /r/${random}`)
+            .setURL(`http://reddit.com/${random}`)
+            .setTimestamp()
+            .setFooter('Valhalla', 'https://i.imgur.com/G5bui5n.png')
 
+        message.channel.send(embed);
+
+        usedCommand.add(message.author.id);
+        setTimeout(() => {
+
+        }, 3000);
+    }
 }
 
-module.exports.config = {
+module.exports.config - {
     name: "meme",
-    description: "",
     usage: "??meme",
+    description: "Sends a meme",
     accessableby: "Members",
     aliases: []
 }
