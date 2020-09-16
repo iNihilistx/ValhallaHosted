128c95cee2da6cf704ff7afb33e05d7fd8074322
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-16 22:51:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ bot.on("message", async message => {
 
     if (cmd === '??poll') {
         if (usedCommand.has(message.author.id)) {
-            message.delete(1000);
+            message.channel.bulkDelete(deleteAmount + 1, true);
             (await message.reply("You are currently in a cooldown. Wait 10 seconds and try again...")).attachments(m => m.delete({ timeout: 5000 }))
         } else {
 
