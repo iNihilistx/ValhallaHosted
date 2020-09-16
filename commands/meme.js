1fb308a80699e99ceb3732c2c13a00a8561b84cf
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-16 23:52:40 +0100

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
     const img = await randomPuppy(random);
 
     if (usedCommand.has(message.author.id)) {
-        message.reply('You are currently in a cooldown. Wait 15 seconds then try again...')
+        message.reply('You are currently in a cooldown. Wait 15 seconds then try again...').then(m => m.delete({ timeout: 5000 }))
         message.delete()
         return;
     } else {
