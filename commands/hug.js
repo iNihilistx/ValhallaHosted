a7075e7ad65cfc7e4d2b56b9a9fd50a98e48e8fa
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-17 22:03:17 +0100

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -17,7 +17,7 @@ module.exports.run = async (bot, message, args) => {
             try {
                 const hugEmbed = new Discord.MessageEmbed()
                     .setColor('#FFA500')
-                    .setTitle(`A Hug Has Been Tightly Sent`)
+                    .setTitle(`A Tight Hug Has Been Sent`)
                     .setURL('https://i.imgur.com/G5bui5n.png')
                     .setImage(image)
                     .setTimestamp()
