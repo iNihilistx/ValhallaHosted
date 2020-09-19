0aa6bd053559108758c3aa2980d43a80d4693baf
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-19 15:15:14 +0100

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -1,41 +1,32 @@
 const usedCommand = new Set();
 const Discord = require('discord.js');
-const botconfig = require('../config.json');
 
 module.exports.run = async (bot, message, args) => {
+
     if (usedCommand.has(message.author.id)) {
         message.reply('You are currently in a cooldown. Wait 15 seconds before trying this command again...').then(m => m.delete({ timeout: 5000 }))
         message.delete()
         return;
     } else {
-        const helpEmbed = new Discord.MessageEmbed()
-            .setTitle('My Commands are: ')
-            .addField("server")
-            .addField("member")
-            .addField("mute <@>")
-            .addField("unmute <@>")
-            .addField("kick <@>")
-            .addField("ban <@>")
-            .addField("poll <#> <text>")
-            .addField("meme")
-            .addField("uwu")
-            .addField("wholesome")
-            .addField("hug <@>")
-            .addField("aesthetic")
-            .addField("cursed")
+        const embed = new Discord.MessageEmbed()
+            .setTitle("Valhallas Commands: ")
+            .description("My commmads are: ")
+            .addField("Server")
             .setTimestamp()
-            .setFooter('Valhalla', 'https://i.imgur.com/G5bui5n.png')
-        message.channel.send(helpEmbed);
+            .setFooter("Valhalla", 'https://i.imgur.com/G5bui5n.png')
+
+        message.channel.send(embed);
+
         usedCommand.add(message.author.id);
         setTimeout(() => {
             usedCommand.delete(message.author.id);
-        }, 15000)
+        }, 15000);
     }
 }
 
 module.exports.config = {
-    name: "Help",
-    description: "shows all the commands for the bot",
+    name: "commands",
+    description: "sends all the commands",
     usage: "??commands",
     accessableby: "Members",
     aliases: []
