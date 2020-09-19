c35cf3eec22adf29925dbbb0c3184b8b96ab971c
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-19 15:39:12 +0100

    sd

--- index.js
+++ index.js
@@ -1,56 +1,11 @@
 const Discord = require('discord.js');
-const botsettings = require('./config.json');
-const usedCommand = new Set();
+const botsettings = require('./botsettings.json');
 
 const bot = new Discord.Client({ disableEveryone: true });
-
-bot.on("guildMemberAdd", member => {
-    const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'welcome')
-
-    welcomeChannel.send(`${member} has joined the cult`)
-})
-
-bot.on("guildMemberRemove", member => {
-    const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === "goodbye")
-    welcomeChannel.send(` ${member} has left the cult`)
-})
-
-bot.on("message", async message => {
-    if (message.author.bot || message.channel.type === "dm") return;
-
-    const messageArray = message.content.split(' ');
-    const cmd = messageArray[0];
-    const args = messageArray.slice(1);
-
-    if (cmd === '??poll') {
-        if (usedCommand.has(message.author.id)) {
-            (await message.reply("You are currently in a cooldown. Wait 10 seconds and try again...")).attachments(m => m.delete({ timeout: 5000 }))
-        } else {
-
-
-            let pollChannel = message.mentions.channels.first();
-            let pollDescription = args.slice(1).join(' ');
-
-            let embedPoll = new Discord.MessageEmbed()
-                .setTitle(`🗳️ New Poll For: ${message.mentions.guild} 🗳️`)
-                .setDescription(pollDescription)
-                .setColor('ORANGE')
-            let msgEmbed = await pollChannel.send(embedPoll);
-            await msgEmbed.react('✅')
-            await msgEmbed.react('❌')
-        }
-
-        usedCommand.add(message.author.id);
-        setTimeout(() => {
-            usedCommand.delete(message.author.id);
-        }, 5000);
-    }
-})
 
 require("./util/eventHandler")(bot)
 
 const fs = require("fs");
-const { isContext } = require('vm');
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 
