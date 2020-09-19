5cb89e389b6bf8795671c407a46d89f3bd3bd403
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-19 02:39:52 +0100

    Create text-channel-create.js

---
+++ commands/text-channel-create.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const botconfig = require('../config.json');
+
+module.exports.run = async (bot, message, args) => {
+    const name = message.content.replace('??maketextchannel ', '')
+    message.guild.channels
+        .create(name, {
+            type: 'text',
+        })
+        .then((channel) => {
+            const categoryId = '737386428144812163'
+            channel.setParent(categoryId)
+        })
+}
+
+module.exports.config = {
+    name: "maketextchannel",
+    usage: "??maketextchannel",
+    description: "makes a new text channel",
+    aliases=[]
+}
