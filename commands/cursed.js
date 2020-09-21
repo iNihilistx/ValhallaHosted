b2eb22a837c994114b575ec439cfdebc78b93fcf
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-21 05:03:38 +0100

    Update cursed.js

--- commands/cursed.js
+++ commands/cursed.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const randomPuppy = require('random-puppy');
 
 module.exports.run = async (bot, message, args) => {
-    const subReddits = ["cursedimages", "blursedimages"]
+    const subReddits = ["cursedimages", "blursedimages", "hmmm"]
     const random = subReddits[Math.floor(Math.random() * subReddits.length)];
     const img = await randomPuppy(random);
 
