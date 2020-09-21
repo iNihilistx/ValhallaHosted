52301cdc73524f19143ffaddcc6bede41d34d31d
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-21 01:42:40 +0100

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -3,7 +3,7 @@ module.exports.run = async (bot, message, args) => {
     let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" ") || x.user.username === args[0])
     if (member.hasPermission(['KICK_MEMBERS', 'BAN_MEMBERS']) && !message.member.hasPermission('MODERATOR')) return;
 
-    let mutedRole = message.guild.roles.cache.get('744306163855786065');
+    let mutedRole = message.guild.roles.cache.get('mute');
     if (mutedRole) {
         member.roles.add(mutedRole);
         message.channel.send("User has been muted.");
