33ca34f242b7e3c658c237b5c976bb02174c5829
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-21 01:53:48 +0100

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -1,14 +1,43 @@
-module.exports.run = async (bot, message, args) => {
-    if (!message.member.hasPermission(['MUTE_MEMBERS'])) return;
-    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" ") || x.user.username === args[0])
-    if (member.hasPermission(['KICK_MEMBERS', 'BAN_MEMBERS']) && !message.member.hasPermission('MODERATOR')) return;
+module.exports.run = async (bot, message, args, con, cfg) => {
+    if (!bot.hasPermission(message, "MANAGE_MESSAGES")) return message.channel.send("You do not have permission to do this.");
 
-    let mutedRole = message.guild.roles.cache.get('muted');
-    let verifiedRole = message.guild.roles.cache.get('dudes');
-    if (mutedRole) {
-        member.roles.add(mutedRole);
-        member.roles.remove(verifiedRole);
-        message.channel.send("User has been muted.");
+    let target = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if (!target) return message.channel.send("Please provide a valid mention or ID.");
+
+    let role = message.guild.roles.find(r => r.name === cfg.mutedRole);
+    if (!role) {
+        try {
+            role = await message.guild.createRole({
+                name: cfg.mutedRole,
+                color: "#354856",
+                permissions: []
+            });
+        } catch (e) {
+            message.channel.send("Error: I cannot create roles.");
+        }
+    }
+
+    if (target.roles.has(role.id)) return message.channel.send("This user is already muted.");
+
+    try {
+        await Promise.all(message.guild.channels.map(async c => {
+            await c.overwritePermissions(role, {
+                SEND_MESSAGES: false,
+                ADD_REACTIONS: false
+            });
+        }));
+
+        let time = parseInt(args[1]);
+        if (time) {
+            con.run("INSERT INTO mutes (snowflake, guild, unmutedAt) VALUES (?, ?, ?)", target.id, message.guild.id, Date.now() + time * 1000, (err) => {
+                if (err) throw err;
+            });
+        }
+
+        await target.addRole(role);
+        return message.channel.send("User muted.");
+    } catch (e) {
+        message.channel.send(`Error: ${e.message}`);
     }
 }
 
