e4c70cb3d515c1746ccd9ba8dffd32e10e5381c3
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-21 01:49:50 +0100

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -1,48 +1,15 @@
-const Discord = require("discord.js");
-const ms = require("ms");
-
 module.exports.run = async (bot, message, args) => {
+    if (!message.member.hasPermission(['MUTE_MEMBERS'])) return;
+    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" ") || x.user.username === args[0])
+    if (member.hasPermission(['KICK_MEMBERS', 'BAN_MEMBERS']) && !message.member.hasPermission('MODERATOR')) return;
 
-    //!mute @user 1s/m/h/d
-
-    let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if (!tomute) return message.channel.send("Please tag user to mute!");
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Sorry, you don't have permissions to use this!");
-    if (tomute.hasPermission("MANAGE_MESSAGES")) return message.channel.send("I cant mute this user");
-    if (tomute.id === message.author.id) return message.channel.send("You cannot mute yourself!");
-    let muterole = message.guild.roles.cache.find(`name`, "Muted");
-
-    if (!muterole) {
-        try {
-            muterole = await message.guild.createRole({
-                name: "Mute",
-                color: "#000000",
-                permissions: []
-            })
-            message.guild.channels.forEach(async (channel, id) => {
-                await channel.overwritePermissions(muterole, {
-                    SEND_MESSAGES: false,
-                    ADD_REACTIONS: false
-                });
-            });
-        } catch (e) {
-            console.log(e.stack);
-        }
+    let mutedRole = message.guild.roles.cache.get('744306163855786065');
+    let verifiedRole = message.guild.roles.cache.get('737386427637170239)');
+    if (mutedRole) {
+        member.roles.add(mutedRole);
+        member.roles.remove(verifiedRole);
+        message.channel.send("User has been muted.");
     }
-
-    let mutetime = args[1];
-    if (!mutetime) return message.channel.send("You didn't specify a time!");
-
-    await (tomute.addRole(muterole.id));
-    message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
-
-    setTimeout(function () {
-        tomute.removeRole(muterole.id);
-        message.channel.send(`<@${tomute.id}> has been unmuted!`);
-    }, ms(mutetime));
-
-    message.delete();
-
 }
 
 module.exports.config = {
