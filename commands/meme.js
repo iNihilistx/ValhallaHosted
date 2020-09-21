e78df64976f05ba7a1bca31acef5e5c1f5d21d04
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-21 04:32:47 +0100

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const randomPuppy = require('random-puppy');
 
 module.exports.run = async (bot, message, args) => {
-    const subReddits = ["meme", "me_irl", "dankmeme", "memes", "raimimemes", "lastimages"]
+    const subReddits = ["meme", "me_irl", "dankmeme", "memes", "raimimemes"]
     const random = subReddits[Math.floor(Math.random() * subReddits.length)];
     const img = await randomPuppy(random);
 
