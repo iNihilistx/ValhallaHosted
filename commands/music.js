8c1dd3fec89195a597517dcd082ec959473bb7a8
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-24 17:11:05 +0100

    Update music.js

--- commands/music.js
+++ commands/music.js
@@ -5,29 +5,13 @@ const { Player } = require("discord-player");
 const player = new Player(bot);
 bot.player = player;
 
-bot.on("message", async message => {
-    let prefix = '!'
+module.exports.run = async (bot, message, args) => {
+    let track = await bot.player.play(message.member.voice.channel, args[0], message.member.user.tag);
+    message.channel.send(`Currently playing ${track.name}! Requested by: ${track.requestedBy}`);
+}
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-
-    if (command === 'play') {
-        let track = await bot.player.play(message.member.voice.channel, args[0], message.member.user.tag);
-        message.channel.send(`Currently Playing ${track.name}! - Requested by ${track.requestedBy}`);
-    }
-
-    if (command === 'stop') {
-        let track = await bot.player.stop(message.guild.id);
-        message.channel.send(`STOPPED PLAYING`);
-    }
-
-    if (command === 'pause') {
-        const track = await bot.player.pause(message.guild.id);
-        message.channel.send(`${track.name} has been paused!`);
-    }
-
-    if (command === 'resume') {
-        const track = await bot.player.resume(message.guild.id);
-        message.channel.send(`${track.name} has resumed playing!`);
-    }
-})
+module.exports.config = {
+    name: "play",
+    usage: "??play",
+    description: "Allows the bot to join the call and play music"
+}
