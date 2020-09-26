0991a72261b4609e56efe5d2757e8ff8911adf25
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-26 05:23:48 +0100

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -8,26 +8,74 @@ module.exports.run = async (bot, message, args) => {
         message.delete()
         return;
     } else {
-        const helpEmbed = new Discord.MessageEmbed()
-            .setColor('#FFA500')
-            .setTitle('Commands for Valhalla with usage: ', true)
-            .addField("server", "??server")
-            .addField('member <@>', "??member @exampleuser", true)
-            .addField('purge <1-99>', "??purge 1-99")
-            .addField('mute <@>', "?? mute @exampleuser")
-            .addField('unmute <@>', "?? unmute @exampleuser", true)
-            .addField('kick <@>', "??kick @exampleuser")
-            .addField('ban <@>', "??ban @exampleuser", true)
-            .addField('poll <#> <text>', "??poll #pollchannel this is a poll")
-            .addField('meme', "??meme", true)
-            .addField('uwu', "??uwu")
-            .addField('wholesome', "??wholesome", true)
-            .addField('hug <@>', "??hug @exampleuser")
-            .addField('cursed', "??cursed", true)
-            .setTimestamp()
-            .setFooter("Valhalla", 'https://i.imgur.com/6jJ425v.png')
-
-        message.channel.send(helpEmbed)
+        message.channel.send({
+            embed: {
+                author: {
+                    name: bot.user.username,
+                    icon_url: clientInformation.user.avatarURL
+                },
+                title: "The commands for valhalla: ",
+                description: "All of the available commands for [??]Valhalla",
+                fields: [{
+                    name: "??server",
+                    value: "??server (displays all information for the guild)"
+                },
+                {
+                    name: "??member",
+                    value: "??member <@user> (Displays information regarding the mentioned user)"
+                },
+                {
+                    name: "??purge",
+                    value: "??purge <1-99> (Allows messages from 1-99 to be deleted in bulk)"
+                },
+                {
+                    name: "??mute",
+                    value: "??mute <@user> (Adds the mute role and removes the ability for voice communication)"
+                },
+                {
+                    name: "??unmute",
+                    value: "??unmute <@> (Takes away the mute role and reinstates the ability for voice communication)"
+                },
+                {
+                    name: "??kick",
+                    value: "??kick <@user> (Allows a user to be kicked from the guild)"
+                },
+                {
+                    name: "??ban",
+                    value: "??ban <@user> (Allows a user to be banned from the guild)"
+                },
+                {
+                    name: "??poll",
+                    value: "??poll <#poll-channel> <text> (Allows a user to remotely start a poll within the poll channel)"
+                },
+                {
+                    name: "??meme",
+                    value: "??meme (Sends a meme to the channel where the command was sent)"
+                },
+                {
+                    name: "??uwu",
+                    value: "??uwu (Sends a gif of an uwu to the channel where the command was sent"
+                },
+                {
+                    name: "??wholesome",
+                    value: "??wholesome (Sends a wholesome image to the channel where the command was sent"
+                },
+                {
+                    name: "??hug",
+                    value: "??hug <@user> (Allows a user to send a hug to the mentioned member"
+                },
+                {
+                    name: "??cursed",
+                    value: "??cursed (sends a cursed image to the channel where the command was sent"
+                }
+                ],
+                timestamp: new Date(),
+                footer: {
+                    icon_url: bot.user.avatarURL,
+                    text: "© Valhalla"
+                }
+            }
+        });
 
         usedCommand.add(message.author.id);
         setTimeout(() => {
