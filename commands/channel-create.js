a05a2863833f244a1702823b77759e86a313adbd
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-27 02:33:14 +0100

    Update channel-create.js

--- commands/channel-create.js
+++ commands/channel-create.js
@@ -1,18 +1,7 @@
-module.exports.run = async (message, args) => {
-    guild.channels.create('new-general', { reason: 'Needed a cool new channel' })
-        .then(console.log)
-        .catch(console.error);
+const Discord = require('discord.js');
 
-    // Create a new channel with permission overwrites
-    guild.channels.create('new-voice', {
-        type: 'voice',
-        permissionOverwrites: [
-            {
-                id: message.author.id,
-                deny: ['VIEW_CHANNEL'],
-            },
-        ],
-    })
+module.exports.run = async (message, args) => {
+
 }
 
 module.exports.config = {
