fa54f0855653845c06084348e7c952cb5b960db3
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-09-27 03:07:54 +0100

    Update channel-create.js

--- commands/channel-create.js
+++ commands/channel-create.js
@@ -1,9 +1,12 @@
 const Discord = require('discord.js');
 module.exports.run = (bot, message) => {
     const args = message.content.slice(15);
-    message.guild.channels.create(`${args}`).then(channel => {
-        channel.setTopic(`New Channel!`)
-    })
+    if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("You lack the permissions needed for this command!");
+    else {
+        message.guild.channels.create(`${args}`).then(channel => {
+            channel.setTopic(`New Channel!`)
+        })
+    }
 }
 
 module.exports.config = {
