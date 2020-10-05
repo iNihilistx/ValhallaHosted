88fc0b6eedd1e35d97d81cfad708edef6520f8be
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-10-05 18:59:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,31 @@ bot.on("guildMemberAdd", member => {
     welcomeChannel.send(`${member} has joined the server!`)
 })
 
-bot.on("guildMemberRemove", member => {
+bot.on("message", async message => {
 
-    const goodbyeChannel = member.guild.channels.cache.find(channel => channel.name === "goodbye")
-    goodbyeChannel.send(` ${member} has left the server!`)
+    const messageArray = message.content.split(' ');
+    const cmd = messageArray[0];
+    const args = messageArray.slice(1);
+
+    if (cmd === '??unban') {
+        if (!message.member.hasPermission("BAN_MEMBERS")) {
+            return message.channel.send(`**${message.author.username}**, You do not have perms to unban someone`)
+        }
+
+        if (!message.guild.me.hasPermission("BAN_MEMBERS")) {
+            return message.channel.send(`**${message.author.username}**, I do not have perms to unban someone`)
+        }
+
+        let userID = args[0]
+        message.guild.fetchBans().then(bans => {
+            if (bans.size == 0) return
+            let bUser = bans.find(b => b.user.id == userID)
+            if (!bUser) return
+            message.guild.members.unban(bUser.user)
+        })
+
+            ;
+    }
 })
 
 bot.on("message", async message => {
