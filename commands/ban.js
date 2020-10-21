ab84627005271649092c9209d5dacfce36d4ec59
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-10-21 14:38:20 +0100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,16 +1,29 @@
 const usedCommand = new Set();
 
 module.exports.run = async (bot, message, args) => {
-	if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Invalid Permissions")
-let User = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-if (!User) return message.channel.send("Invalid User")
-if (User.hasPermission("BAN_MEMBERS")) return message.reply("Invalid Permissions")
-let banReason = args.join(" ").slice(22);
-if (!banReason) {
-  banReason = "None"
-}
+	if (!message.member.hasPermission('BAN_MEMBERS')) {
+		message.reply("You do not have the required permissions needed for this command!").then(m => m.delete({ timeout: 6000 }))
+		message.delete()
+		return;
+	} else {
+		let member = message.mentions.members.first();
+		if (member) {
+			try {
+				await member.ban();
+				console.log(member.tag + ` has been banned from: ${message.guild.name}!`);
+				message.channel.send(`${member}, has been banned from:  ${message.guild.name}!`)
+			}
+			catch (err) {
+				console.log(err);
+			}
+		}
 
-User.ban({reason: banReason})
+	}
+
+	usedCommand.add(message.author.id);
+	setTimeout(() => {
+		usedCommand.delete(message.author.id);
+	}, 6000);
 }
 module.exports.config = {
 	name: "ban",
