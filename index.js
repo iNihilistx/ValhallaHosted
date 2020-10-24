4d96ec07e6f1e66610b3ade2d6d5440617d32719
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-10-24 01:36:41 +0100

    updates

--- index.js
+++ index.js
@@ -51,6 +51,25 @@ bot.on('guildCreate', guild => {
     })
 })
 
+bot.on('message', message => {
+    let args = message.content.substring(PREFIX.length).split(' ');
+    switch(args[0]){
+        case 'report':
+            message.delete({timeout: 3000});
+            let target = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+            if(!target) return message.channel.send('Please provide a user that you wish to report').then(m => m.delete({timeout: 15000}));
+  
+            let reason = args.slice(2).join(" ");
+            if(!reason) return message.channel.send(`Please provide a reason for reporting **${target.user.username}**`).then(m => m.delete({timeout: 15000}));
+  
+            let reportChannel = message.guild.channels.cache.find(x => x.name === "logs");
+  
+            message.channel.send('Your report has been filed to the staff team. Thank you for reporting!').then(m => m.delete({timeout: 15000}));
+            reportChannel.send(`**${message.author.username}** has reported **${target.user.username}** for **${reason}**.`);
+        break;
+      };
+  });
+
 bot.on("message", async message => {
     if (message.author.bot || message.channel.type === "dm") return;
 
@@ -107,6 +126,7 @@ fs.readdir("./commands/", (err, files) => {
         });
     });
 });
+
 
 bot.on("message", async message => {
     if (message.author.bot || message.channel.type === "dm") return;
