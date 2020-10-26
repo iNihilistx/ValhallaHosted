9aef095798675cdbd6a194967002be4cb6b71f7a
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-10-26 19:54:07 +0000

    ban changes

---
+++ ban.js
@@ -0,0 +1,34 @@
+const usedCommand = new Set();
+
+module.exports.run = async (bot, message, args) => {
+    if (!message.member.hasPermission('BAN_MEMBERS')) {
+        message.reply("You do not have the required permissions needed for this command!").then(m => m.delete({ timeout: 6000 }))
+        message.delete()
+        return;
+    } else {
+        let member = message.mentions.members.first();
+        if (member) {
+            try {
+                await member.ban();
+                console.log(member.tag +  has been banned from: ${message.guild.name}!);
+                message.channel.send(${member}, has been banned from:  ${message.guild.name}!)
+            }
+            catch (err) {
+                console.log(err);
+            }
+        }
+
+    }
+
+    usedCommand.add(message.author.id);
+    setTimeout(() => {
+        usedCommand.delete(message.author.id);
+    }, 6000);
+}
+module.exports.config = {
+    name: "ban",
+    description: "",
+    usage: "??ban",
+    accessableby: "Moderators",
+    aliases: []
+}
--- index.js
+++ index.js
@@ -54,23 +54,9 @@ bot.on('guildCreate', guild => {
 bot.on("message", async message => {
     if (message.author.bot || message.channel.type === "dm") return;
 
-    let messageArray = message.content.split(" ")
-    let args = messageArray.slice(1);
-
-    let cmd = messageArray[0];
-
-    if(cmd === "?ban") {
-        let toBan = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(" ") || x.user.username === args[0]);
-
-        if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You need permissions!") 
-        if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send("Bot need permissions!") 
-
-        const reason = args[1] || "There was no reason!";
-
-        toBan.ban({
-            reason: reason
-        })
-        
+    const messageArray = message.content.split(' ');
+    const cmd = messageArray[0];
+    const args = messageArray.slice(1);
 
     if (cmd === '??poll') {
         if (usedCommand.has(message.author.id)) {
