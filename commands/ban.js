cc7c659f378a8802f49f60e97aba2a5ec65bddda
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-10-26 19:44:15 +0000

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,29 +1,25 @@
 const usedCommand = new Set();
 
 module.exports.run = async (bot, message, args) => {
-	if (!message.member.hasPermission('BAN_MEMBERS')) {
-		message.reply("You do not have the required permissions needed for this command!").then(m => m.delete({ timeout: 6000 }))
-		message.delete()
-		return;
-	} else {
-		let toBan = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(" ") || x.user.username === args[0]);
 
-		if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply("You are lacking the needed perms")
+        let toBan = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(" ") || x.user.username === args[0]);
 
-		const reason = args[1] || "No reason provided";
+        if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You need permissions!") 
+        if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send("Bot need permissions!") 
 
-		toBan.ban({
-			reason: reason
-		})
+        const reason = args[1] || "There was no reason!";
 
-		message.channel.send(`${toBan} has been banned from the server! \n Reason Provided: ${reason}`)
-	}
+        toBan.ban({
+            reason: reason
+        })
+        message.channel.send(`${toBan} has been banned from the server!\nReason: ${reason}`)
+    }
 
 	usedCommand.add(message.author.id);
 	setTimeout(() => {
 		usedCommand.delete(message.author.id);
 	}, 6000);
-}
+	
 module.exports.config = {
 	name: "ban",
 	description: "",
