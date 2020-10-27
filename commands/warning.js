9a1ef6e39132fd2e218693d73f6167c73be34dd4
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-10-27 19:17:18 +0000

    Update warning.js

--- commands/warning.js
+++ commands/warning.js
@@ -1,29 +1,17 @@
 const Discord = require('discord.js');
 
 module.exports.run = async(bot, message, args) => {
-    let perms = message.member.hasPermission("MANAGE_MEMBERS")
-    if(!perms) return message.reply("You lack the permissions needed!")
-
-    let user = message.mentions.members.first()
-    if(!user) return message.reply("You need to mention a user!")
-
-    let reason = args.slice(0).join(' ')
-    if(!reason) reason = "No reason has been provided"
-
-    const warningEmbed = new Discord.MessageEmbed()
-    .setTitle("Warned User | " + user.user.tag)
-    .addField("User Warned", `${user}`)
-    .addField("Warned By: ", `${message.author}`)
-    .addField("Reason: ", reason)
-
-    try {
-        user.send(`${user} You have been warned by: ${message.author} in the server ${message.guild.name} with the reason of ${reason}`)
-    } catch (err) {
-        console.log(err)
-    }
-    message.channel.send(`${user} has been warned for ${reason}`)
-
-    message.channel.send(warningEmbed)
+    if (!message.member.hasPermission('BAN_MEMBERS')) {
+        message.reply("You do not have the required permissions needed for this command!").then(m => m.delete({ timeout: 6000 }))
+        message.delete()
+        return;
+    } else {
+        const member = message.mentions.members.first();
+        const warnEmbed = new Discord.MessageEmbed()
+        .setDescription(`${member} has been warned for ${args.slice(0).join(' ')}`)
+        .setColor('orange')
+        message.channel.send(warnEmbed)
+}
 }
 
 module.exports.config = {
