588eb28d075141c104943789519008be22d4bc33
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-10-27 19:27:42 +0000

    Update index.js

--- index.js
+++ index.js
@@ -52,6 +52,32 @@ bot.on('guildCreate', guild => {
 })
 
 bot.on("message", async message => {
+    let perms = message.member.hasPermission("MANAGE_MEMBERS")
+    if(!perms) return message.reply("You lack the permissions needed!")
+
+    let user = message.mentions.members.first()
+    if(!user) return message.reply("You need to mention a user!")
+
+    let reason = args.slice(0).join(' ')
+    if(!reason) reason = "No reason has been provided"
+
+    const warningEmbed = new Discord.MessageEmbed()
+    .setTitle("Warned User | " + user.user.tag)
+    .addField("User Warned", `${user}`)
+    .addField("Warned By: ", `${message.author}`)
+    .addField("Reason: ", reason)
+
+    try {
+        user.send(`${user} You have been warned by: ${message.author} in the server ${message.guild.name} with the reason of ${reason}`)
+    } catch (err) {
+        console.log(err)
+    }
+    message.channel.send(`${user} has been warned for ${reason}`)
+
+    message.channel.send(warningEmbed)
+})
+
+bot.on("message", async message => {
     if (message.author.bot || message.channel.type === "dm") return;
 
     const messageArray = message.content.split(' ');
