ccd2de625e8bf422ea807251adcb7803e20df7b6
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-10-29 00:25:25 +0000

    Update warning.js

--- commands/warning.js
+++ commands/warning.js
@@ -8,43 +8,17 @@ module.exports.run = async (bot, message, args) => {
         message.delete()
         return;
     } else {
-        let member = message.mentions.members.first();
-        if (member) {
-            try {
-                let userArray = message.content.split(" ");
-                let userArgs = userArray.slice(1);
-                let member = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join(" ")) || message.member;
-        
-                if(member.presence.status === 'dnd') member.presence.status = 'Do Not Disturb';
-                if(member.presence.status === 'online') member.presence.status = 'Online';
-                if(member.presence.status === 'idle') member.presence.status = 'Idle';
-                if(member.presence.status === 'offline') member.presence.status = 'Offline';
-        
-                let x = Date.now() - member.createdAt;
-                let y = Date.now() - message.guild.members.cache.get(member.id).joinedAt;
-                const joined = Math.floor(y / 86400000);
-        
-                const joineddate = moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY");
-                let status = member.presence.status;
-                
-                const warningEmbed = new Discord.MessageEmbed()
-                .setAuthor(member.user.tag, member.user.displayAvatarURL())
-                .setTimestamp()
-                .setColor('#FFA500')
-                .setImage(member.user.displayAvatarURL())
-                .addField("**Warned Member:**", member.user.tag)
-                .addField("**Warned Member's ID:**", member.id)
-                .addField("**Joined the server on:** ", `${joineddate} \n ${joined} day(s) Ago`)
-                .addField("**Action:**", "Warned")
-                .setFooter('Valhalla', 'https://i.imgur.com/G5bui5n.png')
-                
-                message.channel.send(warningEmbed);
-        
-            }
-            catch (err) {
-                console.log(err);
-            }
-        }
+        let user = message.guild.member(message.mentions.user.first()) || message.guild.members.get(args[0])
+        if(!user) return message.channel.send("Invalid User to warn!")
+        let warnreason = args.join(" ").slice(22);
+        if(!warnreason) {
+            warnreason = "None"
+        }   
+        const person = message.mentions.user.first()
+        const embed = new Discord.MesssageEmbed()
+        .setTitle(person.username + " has been warned by: " + message.author.username)
+        .setThumbnail('https://i.imgur.com/G5bui5n.png')
+        message.channel.send(embed)
     }
 }
 module.exports.config = {
