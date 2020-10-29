c0e7c41b07b07107140ee6ecf6dfbe668efb8207
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-10-29 00:28:39 +0000

    Update warning.js

--- commands/warning.js
+++ commands/warning.js
@@ -3,23 +3,18 @@ const moment = require('moment');
 
 module.exports.run = async (bot, message, args) => {
 
-    if(!message.member.hasPermission('KICK_MEMBERS')) {
-        message.reply("You do not have the required permissions needed for this command!").then(m => m.delete({timeout:6000}))
-        message.delete()
-        return;
-    } else {
-        let user = message.guild.member(message.mentions.user.first()) || message.guild.members.get(args[0])
-        if(!user) return message.channel.send("Invalid User to warn!")
-        let warnreason = args.join(" ").slice(22);
-        if(!warnreason) {
-            warnreason = "None"
-        }   
-        const person = message.mentions.user.first()
-        const embed = new Discord.MesssageEmbed()
-        .setTitle(person.username + " has been warned by: " + message.author.username)
-        .setThumbnail('https://i.imgur.com/G5bui5n.png')
-        message.channel.send(embed)
+    if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("Invalid Permissions")
+    let User = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+    if (!User) return message.channel.send("Invalid User")
+    let banReason = args.join(" ").slice(22);
+    if (!banReason) {
+    banReason = "None"
     }
+    const person = message.mentions.users.first()
+    const embed = new discord.MessageEmbed()
+        .setTitle(person.username + " got kicked by " + message.author.username)
+        .setThumbnail("https://www.progressivecombatsystems.com/wp-content/uploads/2019/03/groin-kick.jpg")
+    message.channel.send(embed)
 }
 module.exports.config = {
     name: "warn",
