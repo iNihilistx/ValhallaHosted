185ac4e56e68480a62a8674ecd50251801c6d464
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-10-29 00:17:34 +0000

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
 		message.delete()
 		return;
 	} else {
-		let member = message.mentions.members.first() || await message.guild.members.get(args);
+		let member = message.mentions.members.first() || await message.guild.members.cache.get(args);
 		if (member) {
 			try {
 				await member.kick();
