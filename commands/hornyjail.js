66bc8ad43ea2bd828fd7d48c8719afd3aa10d860
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-11-06 21:20:05 +0000

    Create hornyjail.js

---
+++ commands/hornyjail.js
@@ -0,0 +1,44 @@
+const usedCommand = new Set();
+const Discord = require('discord.js');
+const { duration } = require('moment');
+const botconfig = require('../config.json');
+
+module.exports.run = async (bot, message, args) => {
+    const images = ["https://i.imgur.com/LjpHigu.png"];
+    const image = images[Math.floor(Math.random() * images.length)];
+
+    if (usedCommand.has(message.author.id)) {
+        message.reply("You are currently in a cooldown...").then(m => m.delete({ timeout: 5000 }))
+        message.delete()
+        return;
+    } else {
+        let member = message.mentions.members.first();
+        if (member) {
+            try {
+                const hugEmbed = new Discord.MessageEmbed()
+                    .setColor('#FFA500')
+                    .setURL('https://i.imgur.com/G5bui5n.png')
+                    .setImage(image)
+                    .setTimestamp()
+                    .setFooter("Valhalla", 'https://i.imgur.com/G5bui5n.png')
+
+                message.channel.send(hugEmbed);
+
+                usedCommand.add(message.author.id);
+                setTimeout(() => {
+                    usedCommand.delete(message.author.id);
+                }, 15000)
+            }
+            catch (err) {
+                console.log(err);
+            }
+        }
+    }
+}
+module.exports.config = {
+    name: "bonk",
+    description: "",
+    usage: "??bonk",
+    accessableby: "Members",
+    aliases: []
+}
