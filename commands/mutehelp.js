37e1e30b8848ad600b31b21685e58bce61ce2e1f
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2020-12-02 15:58:40 +0000

    Update mutehelp.js

--- commands/mutehelp.js
+++ commands/mutehelp.js
@@ -11,11 +11,11 @@ module.exports = {
                 title: "Valhalla Mute Tutorial:",
                 fields:[{
                     name: "Mute",
-                    value: "``??mute @member [Time in seconds] [Reason]``"
+                    value: "``??mute @member [Time in seconds][s] [Reason]``"
                 },
                 {
                     name: "How To:",
-                    value: "??mute followed by the @ of the member, followed by length for the mute (in seconds, the max time for a mute being '2592000' which is one month!). The reason is optional, but it allows for people to see why the member has been muted!"
+                    value: "??mute followed by the @ of the member, followed by length for the mute (in seconds, the max time for a mute being '2592000' which is one month!), make sure to include an 's' after the time. The reason is optional, but it allows for people to see why the member has been muted!\n\An example is ??mute @member 10s spamming"
                 },
             ],
             timestamp: new Date(),
