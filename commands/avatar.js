6ee9a3c5ebe9f81f1b82c537e378fe43b7ba2f2b
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2021-01-23 06:54:04 +0000

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -14,6 +14,7 @@ module.exports = {
         } else {
             let userArray = message.content.split("");
             let userArgs = userArray.slice(1);
+            let member = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join(" ") || x.user.username === userArgs[0]) || message.member;
 
 
             const avatarEmbed = new Discord.MessageEmbed()
