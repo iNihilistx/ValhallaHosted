01182ef025476ef34707258b3cadd1acd6ef7ed0
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2021-01-23 21:15:34 +0000

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -7,30 +7,23 @@ module.exports = {
     description: "shows avatar of users!",
     usage: "<member>",
     async execute (message, args) {
-        if(usedCommand.has(message.author.id)){
-            message.reply("You are currently in a cooldown...").then(m => m.delete({timeout: 2000}))
-            message.delete()
-            return;
-        } else {
+        const mentionedMember = message.mentions.members.first()
+        if(!mentionedMember) {
+            return message.reply("You need to mention someone to use this command!")
+        } else{
             let userArray = message.content.split("");
             let userArgs = userArray.slice(1);
-            let member = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join(" ") || x.user.username === userArgs[0]) || message.member;
+            let member = message.mentions.members.first();
 
 
             const avatarEmbed = new Discord.MessageEmbed()
-            .setColor('#00c09a')
             .setTitle("Avatar Showcase:")
             .setAuthor(member.user.tag)
-            .setImage(member.user.displayAvatarURL({format: 'png', size: 2048, dynamic: true}))
+            .setImage(member.user.displayAvatarURL({format: 'png', size: 512}))
             .setTimestamp()
             .setFooter("Sunny", 'https://i.imgur.com/I7mrlPC.png')
 
             message.channel.send(avatarEmbed);
         }
-
-        usedCommand.add(message.author.id);
-        setTimeout(() => {
-            usedCommand.delete(message.author.id);
-        }, 2000);
     }
 }
