b04b8bcb70e52f76a8330b1c322e7e2a8e823e1d
Nairobi <64751372+Naiirobii@users.noreply.github.com>
2021-01-28 01:49:40 +0000

    Update murder.js

--- commands/murder.js
+++ commands/murder.js
@@ -12,13 +12,13 @@ module.exports = {
         var deathnotes = ["was killed with kindness, or an axe... I forget which", "took a bullet point black to the face", "**accidentally** fell down the stairs"];
         var deathnote = Math.floor(Math.random() * deathnotes.length);
 
-        if(usedCommand.has(message.author.id)){
-            message.reply("You are currently in a cooldown...".then(m => m.delete({timeout: 2000})))
+        if(usedCommand.has(message.author.id)) {
+            message.reply("You are currently in a cooldown...").then(m => m.delete({timeout: 2000}))
             message.delete()
             return;
         }
 
-        else if(!mentionedMember) {
+         else if(!mentionedMember) {
             return message.reply("You need to mention a user in order to use the kill command!").then(m => m.delete({timeout:2000}))
             message.delete()
         } else {
